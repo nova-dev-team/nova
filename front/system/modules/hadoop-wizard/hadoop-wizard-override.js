47c8b6852320f8ea0aabfab30a7bc83636204d49
Santa Zhang <santa1987@gmail.com>
2009-05-05 13:54:54 +0800

    Cardlayout

--- front/system/modules/hadoop-wizard/hadoop-wizard-override.js
+++ front/system/modules/hadoop-wizard/hadoop-wizard-override.js
@@ -1,9 +1,19 @@
 Ext.override(QoDesk.HadoopWizard, {
+
+	cards : [
+		'hadoop-wizard-1', // settings
+		'hadoop-wizard-2' // progress
+	],
 	
-	detailModule : null,
+	contentPanel : null,
 	
-	init : function(){
-		this.detailModule = new HadoopWizardDetailModule();
+	layout: null,
+	win : null,
+	
+	bottomBar:null,
+	
+	viewCard : function(card){
+		this.layout.setActiveItem(card);
 	},
 	
 	createWindow : function(){
@@ -11,106 +21,106 @@ Ext.override(QoDesk.HadoopWizard, {
 		var win = desktop.getWindow('hadoop-wizard-win');
 		
 		if(!win){
-            win = desktop.createWindow({
-                autoScroll: true,
-                id: 'hadoop-wizard-win',
-                title: 'Hadoop Wizard Window',
-                width:640,
-                height:480,
-                iconCls: 'hadoop-wizard-icon',
-                items: new QoDesk.HadoopWizard.NavPanel({owner: this, id: 'nav-panel'}),
-                shim:false,
-                animCollapse:false,
-                constrainHeader:true,
-                maximizable: false,
-                tbar: [{
-                	handler: this.showDialog,
-                	scope: this,
-                	text: 'Open Dialog'
-                }],
-                taskbuttonTooltip: '<b>HadoopWizard Window</b><br />A HadoopWizard window'
-            });
-        }
-        
-        win.show();
-    },
-    
-    openDetail : function(id){
-		this.detailModule.createWindow(this.app, id);
-    },
-    
-    showDialog : function(){
-    	var winManager = this.app.getDesktop().getManager();
-    	
-    	if(!this.dialog){
-            this.dialog = new Ext.Window({
-            	bodyStyle:'padding:10px',
-                layout:'fit',
-                width:500,
-                height:300,
-                closeAction:'hide',
-                plain: true,
-                html: 'HadoopWizard dialog window',
-                buttons: [{
-                    text:'Submit',
-                    disabled:true
-                },{
-                    text: 'Close',
-                    handler: function(){
-                        this.dialog.hide();
-                    },
-                    scope: this
-                }],
-                manager: winManager,
-                modal: true
-            });
-        }
-        this.dialog.show();
+		
+	    this.contentPanel = new Ext.Panel({
+			    activeItem: 0,
+          border: false,
+			    id: 'pref-win-content',
+			    items: [
+          	new QoDesk.HadoopWizard.SettingPanel({owner: this, id: 'hadoop-wizard-1'}),
+          	new QoDesk.HadoopWizard.ProgressPanel({owner: this, id: 'hadoop-wizard-2'})
+          ],
+          layout: 'card',
+        });
+		
+		
+      win = desktop.createWindow({
+          autoScroll: true,
+          id: 'hadoop-wizard-win',
+          title: 'Hadoop Wizard Window',
+          width:640,
+          height:480,
+          iconCls: 'hadoop-wizard-icon',
+          items: this.contentPanel,
+          shim:false,
+          layout:"fit",
+          animCollapse:false,
+          constrainHeader:true,
+          maximizable: false,
+          taskbuttonTooltip: '<b>HadoopWizard Window</b><br />A HadoopWizard window'
+      });
+      
+ 			this.layout = this.contentPanel.getLayout();
     }
+        
+    win.show();
+  }  
 });
 
 
 
-QoDesk.HadoopWizard.NavPanel = function(config){
+
+QoDesk.HadoopWizard.SettingPanel = function(config){
 	this.owner = config.owner;
 	
-	QoDesk.HadoopWizard.NavPanel.superclass.constructor.call(this, {
+	QoDesk.HadoopWizard.SettingPanel.superclass.constructor.call(this, {
 		autoScroll: true,
 		bodyStyle: 'padding:15px',
 		border: false,
-		html: '<ul id="hadoop-wizard-nav-panel"> \
+		html: '<ul id="pref-nav-panel"> \
 				<li> \
-					<a id="openDetailOne" href="#">Detail 1</a><br /> \
-					<span>Open detail window one.</span> \
+					<img src="'+Ext.BLANK_IMAGE_URL+'" class="icon-pref-autorun"/> \
+					<a id="viewShortcuts" href="#">Shortcuts</a><br /> \
+					<span>Choose which applications appear in your shortcuts.</span> \
 				</li> \
 				<li> \
-					<a id="openDetailTwo" href="#">Detail 2</a><br /> \
-					<span>Open detail window two.</span> \
+					<img src="'+Ext.BLANK_IMAGE_URL+'" class="icon-pref-autorun"/> \
+					<a id="viewAutoRun" href="#">Auto Run Apps</a><br /> \
+					<span>Choose which applications open automatically once logged in.</span> \
 				</li> \
 				<li> \
-					<a id="openDetailThree" href="#">Detail 3</a><br /> \
-					<span>Open detail window three.</span> \
+					<img src="'+Ext.BLANK_IMAGE_URL+'" class="icon-pref-quickstart"/> \
+					<a id="viewQuickstart" href="#">Quick Start Apps</a><br /> \
+					<span>Choose which applications appear in your Quick Start panel.</span> \
+				</li> \
+				<li> \
+					<img src="'+Ext.BLANK_IMAGE_URL+'" class="icon-pref-appearance"/> \
+					<a id="viewAppearance" href="#">Window Color and Appearance</a><br /> \
+					<span>Fine tune window color and style of your windows.</span> \
+				</li> \
+				<li> \
+					<img src="'+Ext.BLANK_IMAGE_URL+'" class="icon-pref-wallpaper"/> \
+					<a id="viewWallpapers" href="#">Desktop Background</a><br /> \
+					<span>Choose from available wallpapers or colors to decorate you desktop.</span> \
 				</li> \
 			</ul>',
 		id: config.id
 	});
 	
 	this.actions = {
-		'openDetailOne' : function(owner){
-			owner.openDetail(1);
+		'viewShortcuts' : function(owner){
+			owner.viewCard('hadoop-wizard-2');
 		},
 		
-		'openDetailTwo' : function(owner){
-			owner.openDetail(2);
+		'viewAutoRun' : function(owner){
+			owner.viewCard('hadoop-wizard-2');
 		},
 		
-		'openDetailThree' : function(owner){
-	   		owner.openDetail(3);
+		'viewQuickstart' : function(owner){
+	   		owner.viewCard('hadoop-wizard-2');
+	   	},
+	   	
+	   	'viewAppearance' : function(owner){
+	   		owner.viewCard('hadoop-wizard-2');
+	   	},
+	   	
+	   	'viewWallpapers' : function(owner){
+	   		owner.viewCard('hadoop-wizard-2');
 	   	}
 	};
 };
 
-Ext.extend(QoDesk.HadoopWizard.NavPanel, Ext.Panel, {
+Ext.extend(QoDesk.HadoopWizard.SettingPanel, Ext.Panel, {
 	afterRender : function(){
 		this.body.on({
 			'mousedown': {
@@ -126,7 +136,7 @@ Ext.extend(QoDesk.HadoopWizard.NavPanel, Ext.Panel, {
 			}
 		});
 		
-		QoDesk.HadoopWizard.NavPanel.superclass.afterRender.call(this); // do sizing calcs last
+		QoDesk.HadoopWizard.SettingPanel.superclass.afterRender.call(this); // do sizing calcs last
 	},
 	
 	doAction : function(e, t){
@@ -136,42 +146,45 @@ Ext.extend(QoDesk.HadoopWizard.NavPanel, Ext.Panel, {
 });
 
 
-
-HadoopWizardDetailModule = Ext.extend(Ext.app.Module, {
-
-	moduleType : 'demo',
-	moduleId : 'demo-hadoop-wizard-detail',
+QoDesk.HadoopWizard.ProgressPanel = function(config){
+	this.owner = config.owner;
 	
-	init : function(){
-		this.launcher = {
-			handler: this.createWindow,
-			iconCls: 'hadoop-wizard-icon',
-			scope: this,
-			shortcutIconCls: 'demo-hadoop-wizard-shortcut',
-			text: 'HadoopWizard Detail Window',
-			tooltip: '<b>HadoopWizard Detail Window</b><br />A HadoopWizard detail window'
-		}
-	},
+	QoDesk.HadoopWizard.ProgressPanel.superclass.constructor.call(this, {
+		autoScroll: true,
+		bodyStyle: 'padding:15px',
+		border: false,
+		html: 'BAGA',
+		id: config.id
+	});
+	
+	this.actions = {
+		
+	};
+};
 
-	createWindow : function(app, id){
-		this.moduleId = 'demo-hadoop-wizard-detail-'+id;
+
+Ext.extend(QoDesk.HadoopWizard.ProgressPanel, Ext.Panel, {
+	afterRender : function(){
+		this.body.on({
+			'mousedown': {
+				fn: this.doAction,
+				scope: this,
+				delegate: 'a'
+			},
+			'click': {
+				fn: Ext.emptyFn,
+				scope: null,
+				delegate: 'a',
+				preventDefault: true
+			}
+		});
 		
-		var desktop = app.getDesktop();
-		var win = desktop.getWindow('hadoop-wizard-detail'+id);
-		
-        if(!win){
-            win = desktop.createWindow({
-                id: 'hadoop-wizard-detail'+id,
-                title: 'Detail Window '+id,
-                width: 540,
-                height: 380,
-                html : '<p>Something useful would be in here.</p>',
-                iconCls: 'hadoop-wizard-icon',
-                shim:false,
-                animCollapse:false,
-                constrainHeader:true
-            });
-        }
-        win.show();
+		QoDesk.HadoopWizard.ProgressPanel.superclass.afterRender.call(this); // do sizing calcs last
+	},
+	
+	doAction : function(e, t){
+    	e.stopEvent();
+    	this.actions[t.id](this.owner);  // pass owner for scope
     }
 });
+
