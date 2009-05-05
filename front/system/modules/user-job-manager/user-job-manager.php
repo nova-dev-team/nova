722784429d07ada4c4a62485cbf9faacc9dbf57c
Santa Zhang <santa1987@gmail.com>
2009-05-05 12:10:47 +0800

    Going to modify user-job-manager.php

--- front/system/modules/user-job-manager/user-job-manager.php
+++ front/system/modules/user-job-manager/user-job-manager.php
@@ -9,9 +9,6 @@ class UserJobManager {
   }
 
   public function dummyTest() {
-//    	$user_id = $os->session->get_member_id();
-
-    //echo "{success:true, user_id:" . $user_id ."}";
     echo "{success:true, msg:'haha->Member id : " . $this->os->session->get_member_id() ."!!!'}";
   }
 
@@ -19,50 +16,21 @@ class UserJobManager {
   public function listCluster() {
   
     $email = $this->os->session->get_member_email();
-    
-//echo $email;
 
-   $core_reply = file_get_contents("http://localhost:3000/user/info_vclusters/" . $email);
- // echo $core_reply;
-  $lst = json_decode($core_reply);
+    $core_reply = file_get_contents("http://localhost:3000/user/info_vclusters/" . $email);
 
-//  echo sizeof($lst);
-  
-/*  for ($i = 0; $i < sizeof($lst); $i++) {
-    echo $lst[$i];
-}*/
-//   $json = new Services_JSON();
+    $lst = json_decode($core_reply);
 
     echo "{'success':true, 'all_clusters':[";
-    
-    // XXX DUMMY CODE
-    
- /*   if ($_REQUEST["cid"] == "") {
-  
 
-    
-    $total = 6;
-    for ($i = 0; $i < $total; $i++) {
-      echo "{'cluster_id':'c" . $i ."', 'cluster_name':'1.2.3.4'}";
-      
-      if ($i < $total - 1) {
-        echo ",";
+    $total = sizeof($lst);
+    for ($i = 0; $i < $total; $i += 2) {
+      echo "{'cluster_id' :  '" . $lst[$i] . "', cluster_name:'" . $lst[$i + 1] ."'}";
+      if ($i < $total - 2) {
+        echo ',';
       }
     }
-    
-  }*/
-  
-  $total = sizeof($lst);
-//  echo $total;
-  for ($i = 0; $i < $total; $i += 2) {
-    echo "{'cluster_id' :  '" . $lst[$i] . "', cluster_name:'" . $lst[$i + 1] ."'}";
-if ($i < $total - 2) {
-  echo ',';
-}
-}
-  
-  
-      echo "]}";
+    echo "]}";
 
   }
   
@@ -84,17 +52,12 @@ if ($i < $total - 2) {
   public function newVM() {
   
     $add_opt = "mem=" . $_REQUEST['mem'] . "&img=" . $_REQUEST['img'] . "&vcpu=" . $_REQUEST['vcpu'];
-    
-    
     if ($add_opt == "")
       $core_reply = file_get_contents("http://localhost:3000/vcluster/add_new_vm/" . $_REQUEST['vcluster_cid']);
     else
       $core_reply = file_get_contents("http://localhost:3000/vcluster/add_new_vm/" . $_REQUEST['vcluster_cid'] . "?" . $add_opt);
 
     echo $core_reply;
-    
-    
-
   }
   
   public function removeVM() {
@@ -103,51 +66,42 @@ if ($i < $total - 2) {
   }
   
   
-    public function startVM() {
-      $core_reply = file_get_contents("http://localhost:3000/vmachine/start/" . $_REQUEST['vm_vid']);
-      echo $core_reply;
-  }
-  
-  
-  
-    public function stopVM() {
-      $core_reply = file_get_contents("http://localhost:3000/vmachine/stop/" . $_REQUEST['vm_vid']);
-      echo $core_reply;
-  }
-  
-  
-    public function pauseVM() {
-      $core_reply = file_get_contents("http://localhost:3000/vmachine/suspend/" . $_REQUEST['vm_vid']);
-      echo $core_reply;
-  }
-  
-    public function resumeVM() {
-      $core_reply = file_get_contents("http://localhost:3000/vmachine/resume/" . $_REQUEST['vm_vid']);
-      echo $core_reply;
-  }
-  
-    public function infoVM() {
-    
-    
-    $core_reply = file_get_contents("http://localhost:3000/vmachine/detail_info/" . $_REQUEST['vm_id'] );
-    
-//    $obj = json_decode($core_reply);
-//    echo $obj["success"];
-    
+  public function startVM() {
+    $core_reply = file_get_contents("http://localhost:3000/vmachine/start/" . $_REQUEST['vm_vid']);
     echo $core_reply;
-//    echo "{success:true, info:'TODO: Detail info of "   . $_REQUEST['vm_id']  .".', pmip:'" . "10.0.0.210" . "', vnc_port:'" . "5900" ."'  }"; // TODO
-
   }
   
-public function listVM() {
+  
+  
+  public function stopVM() {
+    $core_reply = file_get_contents("http://localhost:3000/vmachine/stop/" . $_REQUEST['vm_vid']);
+    echo $core_reply;
+  }
+  
+  
+  public function pauseVM() {
+    $core_reply = file_get_contents("http://localhost:3000/vmachine/suspend/" . $_REQUEST['vm_vid']);
+    echo $core_reply;
+  }
+  
+  public function resumeVM() {
+    $core_reply = file_get_contents("http://localhost:3000/vmachine/resume/" . $_REQUEST['vm_vid']);
+    echo $core_reply;
+  }
+  
+  public function infoVM() {
+    $core_reply = file_get_contents("http://localhost:3000/vmachine/detail_info/" . $_REQUEST['vm_id'] );
+    // TODO Pretty reply
+    echo $core_reply;
+  }
+  
+  public function listVM() {
     
     $core_reply = file_get_contents("http://localhost:3000/vcluster/info_vm_list/" . $_REQUEST['cid']);
-    //echo $core_reply;
     
     $lst = json_decode($core_reply);
 
     echo "{'success':true, 'all_vms':[";
-    
     
     $total = sizeof($lst);
     for ($i = 0; $i < $total; $i+=4) {
@@ -160,14 +114,12 @@ public function listVM() {
     }
     
     echo "]}";
-  
-}
+  }
 
-public function listImage() {
-
-  echo "{success:true, imglist:['hadoop-slave.img', 'intrepid2.img', 'f8.img']}";
-  // TODO
-}
+  public function listImage() {
+    echo "{success:true, imglist:['hadoop-slave.img', 'intrepid2.img', 'f8.img']}";
+    // TODO get image list from core
+  }
 
 }
 
