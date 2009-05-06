98ecb40e0772ce2561edbd6de96a20cb3e294ea9
Santa Zhang <santa1987@gmail.com>
2009-05-06 12:19:19 +0800

    Updated batch controller

--- core/app/controllers/batch_controller.rb
+++ core/app/controllers/batch_controller.rb
@@ -1,2 +1,53 @@
 class BatchController < ApplicationController
+
+  include BatchHelper
+
+  def create
+    result = BatchHelper::Helper.create params[:id], params[:arg]
+    respond_to do |accept| 
+      accept.html {render :text => result.to_json}
+      accept.json {render :json => result}
+    end
+  end
+
+=begin
+  def list
+    result = BatchHelper::Helper.list
+    
+    respond_to do |accept| 
+      accept.html {render :text => result.to_json}
+      accept.json {render :json => result}
+    end
+  end
+=end
+
+  def change_setting
+    # TODO
+  end
+
+  def add_soft
+    result = BatchHelper::Helper.add_soft params[:id], params[:arg]
+    
+    respond_to do |accept| 
+      accept.html {render :text => result.to_json}
+      accept.json {render :json => result}
+    end
+  end
+
+  def do_install
+    result = BatchHelper::Helper.do_install params[:id]
+    respond_to do |accept| 
+      accept.html {render :text => result.to_json}
+      accept.json {render :json => result}
+    end
+  end
+
+  def progress
+    result = BatchHelper::Helper.progress params[:id]
+    respond_to do |accept| 
+      accept.html {render :text => result.to_json}
+      accept.json {render :json => result}
+    end
+  end
+
 end
--- core/app/helpers/batch_helper.rb
+++ core/app/helpers/batch_helper.rb
@@ -1,2 +1,52 @@
+require 'ftools'
+
 module BatchHelper
+
+  include VclusterHelper
+
+  class Helper
+
+    def Helper.create name, size
+      result = {}
+      sub_result = VclusterHelper::Helper.create name
+      result[:vcluster_cid] = sub_result[:vcluster_cid]
+      File.open ("tmp/#{result[:vcluster_cid]}.txt", "w") do |file|
+        file.write("common\nssh-nopass\n")
+        file.flush()
+        file.close()
+      end
+
+      # TODO write node.list
+      return result
+    end
+
+    def Helper.change_setting vcluster_cid, item, value
+      # TODO
+    end
+
+    def Helper.add_soft vcluster_cid, soft_name
+      result = {:success => true}
+      File.open ("tmp/#{vcluster_cid}.txt", "a") do |file|
+        file.write("#{soft_name}\n")
+      end
+      result[:soft_list] = []
+      File.foreach ("tmp/#{vcluster_cid}.txt") do |line|
+        result[:soft_list] << line.chomp
+      end
+      return result
+    end
+
+    def Helper.do_install vcluster_cid
+      result = {}
+# TODO call hg
+      return result
+    end
+
+    def Helper.progress vcluster_cid
+      result = {}
+# TODO call hg
+      return result
+    end
+
+  end
 end
