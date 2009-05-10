6d7f05a3923e46b268129f0ee9b368b804253a08
Santa Zhang <santa1987@gmail.com>
2009-05-10 22:05:46 +0800

    Enhance Wizard

--- front/system/modules/hadoop-wizard/hadoop-wizard-override.js
+++ front/system/modules/hadoop-wizard/hadoop-wizard-override.js
@@ -20,45 +20,63 @@ Ext.override(QoDesk.HadoopWizard, {
       create_tab1 = new Ext.Panel({
         region: 'west',
         border: false,
-        html:'<img src="/image/wizard.png" />'
+        html:'<img src="/image/wizard.png" width=256 height=256/>'
       });
       
-      create_options = [{
-        fieldLabel: 'Cluster Size',
-        xtype:'textfield',
-        id: 'wizard_opt_csize',
-        value:'4'
-      }, {
-        fieldLabel: 'Cluster Name',
-        xtype:'textfield',
-        value:'Wizard_Cluster',
-        id: 'wizard_opt_cname'
-      }, {
-        fieldLabel: 'Cpu Count',
-        xtype:'textfield',
-        id: 'wizard_opt_vcpu',
-        value: "1"
-      }, {
-        fieldLabel: 'Memory Size',
-        xtype:'textfield',
-        id: 'wizard_opt_mem',
-        value: "512"
-      }, {
-        fieldLabel: 'Install Hadoop',
-        xtype:'checkbox',
-        soft_name: 'hadoop',
-        id: 'wizard_app_hadoop'
-      }, {
-        fieldLabel: 'Install MPI',
-        xtype:'checkbox',
-        soft_name: 'mpi',
-        id: 'wizard_app_mpi'
-      }, {
-        fieldLabel: 'Install Ganglia',
-        xtype:'checkbox',
-        soft_name: 'ganglia',
-        id: 'wizard_app_ganglia'
-      }];
+      create_options = [
+        new Ext.form.FieldSet({
+          title: "Vcluster Settings",
+          autoHeight: true,
+          items: [{
+            fieldLabel: 'Cluster Size',
+            xtype:'textfield',
+            id: 'wizard_opt_csize',
+            value:'4'
+          }, {
+            fieldLabel: 'Cluster Name',
+            xtype:'textfield',
+            value:'Wizard_Cluster',
+            id: 'wizard_opt_cname'
+          }]
+        }),
+        
+        new Ext.form.FieldSet({
+          title: "Vmachine Settings",
+          autoHeight: true,
+          items: [{
+            fieldLabel: 'Cpu Count',
+            xtype:'textfield',
+            id: 'wizard_opt_vcpu',
+            value: "1"
+          }, {
+            fieldLabel: 'Memory Size',
+            xtype:'textfield',
+            id: 'wizard_opt_mem',
+            value: "512"
+          }]
+        }),
+        
+        new Ext.form.FieldSet({
+          title: "Softwares",
+          autoHeight: true,
+          items: [{
+            fieldLabel: 'Install Hadoop',
+            xtype:'checkbox',
+            soft_name: 'hadoop',
+            id: 'wizard_app_hadoop'
+          }, {
+            fieldLabel: 'Install MPI',
+            xtype:'checkbox',
+            soft_name: 'mpi',
+            id: 'wizard_app_mpi'
+          }, {
+            fieldLabel: 'Install Ganglia',
+            xtype:'checkbox',
+            soft_name: 'ganglia',
+            id: 'wizard_app_ganglia'
+          }]
+        })
+      ];
       
       create_tab2 = new Ext.FormPanel({
         region: 'center',
@@ -72,73 +90,69 @@ Ext.override(QoDesk.HadoopWizard, {
         buttons: [{
           text: "Create",
           handler: function() {
+            clu_size = Ext.get("wizard_opt_csize").dom.value;
+            clu_name = Ext.get("wizard_opt_cname").dom.value;
+
+            mem_size_val = Ext.get("wizard_opt_mem").dom.value;
+            vcpu_count = Ext.get("wizard_opt_vcpu").dom.value;
+      
+            if (clu_name.indexOf(" ") != -1 || clu_name.indexOf("\t") != -1) {
+              alert("Space is not allowed in cluster name!");
+              return;
+            }
+      
+            soft_list = "";
+            if (Ext.get("wizard_app_mpi").dom.checked) {
+              soft_list += "mpi\n";
+            }
             
-      clu_size = Ext.get("wizard_opt_csize").dom.value;
-      clu_name = Ext.get("wizard_opt_cname").dom.value;
-
-        mem_size_val = Ext.get("wizard_opt_mem").dom.value;
-        vcpu_count = Ext.get("wizard_opt_vcpu").dom.value;
-      
-      if (clu_name.indexOf(" ") != -1 || clu_name.indexOf("\t") != -1) {
-        alert("Space is not allowed in cluster name!");
-        return;
-      }
-      
-      soft_list = "";
-      if (Ext.get("wizard_app_mpi").dom.checked) {
-        soft_list += "mpi\n";
-      }
-      
-      if (Ext.get("wizard_app_hadoop").dom.checked) {
-        soft_list += "hadoop\n";
-      }
-      
-      if (Ext.get("wizard_app_ganglia").dom.checked) {
-        soft_list += "ganglia\n";
-      }
-      
-      var notifyWin = desktop.showNotification({
-          html: 'Sending request...',
-          title: 'Please wait'
-      });
-      
-      Ext.Ajax.request({
-        url: '/connect.php',
-        params: {
-          moduleId: 'hadoop-wizard',
-          action: "create",
-          vcluster_name: clu_name,
-          vcluster_size: clu_size,
-          software_list: soft_list,
-          mem_size: mem_size_val,
-          vcpu: vcpu_count,
-          app_name: "hadoop"
-        },
+            if (Ext.get("wizard_app_hadoop").dom.checked) {
+              soft_list += "hadoop\n";
+            }
+            
+            if (Ext.get("wizard_app_ganglia").dom.checked) {
+              soft_list += "ganglia\n";
+            }
+            
+            var notifyWin = desktop.showNotification({
+                html: 'Sending request...',
+                title: 'Please wait'
+            });
+            
+            Ext.Ajax.request({
+              url: '/connect.php',
+              params: {
+                moduleId: 'hadoop-wizard',
+                action: "create",
+                vcluster_name: clu_name,
+                vcluster_size: clu_size,
+                software_list: soft_list,
+                mem_size: mem_size_val,
+                vcpu: vcpu_count,
+                app_name: "hadoop"
+              },
         
-        success: function(o){
-          if (o && o.responseText &&  o.responseText != "" && Ext.decode(o.responseText).success) {
-              obj = Ext.decode(o.responseText);
-              saveComplete('Finished', "Created new cluster " + obj.cid + "(" + obj.cname + ")");
-          }
-          else {
-              saveComplete('Error', 'Errors encountered on the server.');
-          }
-        },
-        failure: function(){
-          saveComplete('Error', 'Lost connection to server.');
-        }
-      });
-
-
+              success: function(o){
+                if (o && o.responseText &&  o.responseText != "" && Ext.decode(o.responseText).success) {
+                    obj = Ext.decode(o.responseText);
+                    saveComplete('Finished', "Created new cluster " + obj.cid + "(" + obj.cname + ")");
+                }
+                else {
+                    saveComplete('Error', 'Errors encountered on the server.');
+                }
+              },
+              failure: function(){
+                saveComplete('Error', 'Lost connection to server.');
+              }
+            });
 
             function saveComplete(title, msg){
-                notifyWin.setIconClass('x-icon-done');
-                notifyWin.setTitle(title);
-                notifyWin.setMessage(msg);
-                desktop.hideNotification(notifyWin);
+              notifyWin.setIconClass('x-icon-done');
+              notifyWin.setTitle(title);
+              notifyWin.setMessage(msg);
+              desktop.hideNotification(notifyWin);
             }            
-            
-            
+
           }
         }, {
           text: "Exit",
@@ -148,15 +162,12 @@ Ext.override(QoDesk.HadoopWizard, {
         }]
       });
       
-      
-      
       create_tab = new Ext.Panel({
         layout:'border',
         title: "Create new cluster",
         border: false,
         items:[create_tab1, create_tab2]
       });
-      
       
       var progressPane = new Ext.Panel({
         title: "Deploy Progress",
@@ -172,90 +183,89 @@ Ext.override(QoDesk.HadoopWizard, {
       function progress_tab_refresh() {
         my_div = Ext.get("oh_my_crappy_div_should_work");
         
-        
-      Ext.Ajax.request({
-        url: '/connect.php',
-        params: {
-          moduleId: 'hadoop-wizard',
-          action: "progress"
-        },
-        
-        success: function(o){
-          if (o && o.responseText != null &&  o.responseText != "" && Ext.decode(o.responseText).success) {
-            var html = "<center><p><p>";
-            progress_list = Ext.decode(o.responseText).progress;
-            for (i = 0; i < progress_list.length; i++) {
-              cid = progress_list[i].vcluster_cid;
-              cname = progress_list[i].vcluster_cname;
-              html += "Progress of virtual cluster " + cid + "(" + cname + ")<br>";
-              html += "<table class='hadoop-wizard-progresstable'>";
-              
-              progrs_info = Ext.decode(progress_list[i].progress_info);
-              
-              vm_count = progrs_info.length;
-              app_count = progrs_info[0].progress.length;
-              
-              html += "<tr>";
-              cluster_progress = "finished";
-              for (vm_i = 0; vm_i < vm_count; vm_i++) {
-                vm_progress = "finished";
+        Ext.Ajax.request({
+          url: '/connect.php',
+          params: {
+            moduleId: 'hadoop-wizard',
+            action: "progress"
+          },
+          
+          success: function(o){
+            if (o && o.responseText != null &&  o.responseText != "" && Ext.decode(o.responseText).success) {
+              var html = "<center><p><p>";
+              progress_list = Ext.decode(o.responseText).progress;
+              for (i = 0; i < progress_list.length; i++) {
+                cid = progress_list[i].vcluster_cid;
+                cname = progress_list[i].vcluster_cname;
+                html += "Progress of virtual cluster " + cid + "(" + cname + ")<br>";
+                html += "<table class='hadoop-wizard-progresstable'>";
                 
-                for (k = 0; k < app_count; k++) {
-                  if (progrs_info[vm_i].progress[k].indexOf("Waiting") != -1) {
-                    vm_progress = "waiting";
-                    break;
+                progrs_info = Ext.decode(progress_list[i].progress_info);
+                
+                vm_count = progrs_info.length;
+                app_count = progrs_info[0].progress.length;
+                
+                html += "<tr>";
+                cluster_progress = "finished";
+                for (vm_i = 0; vm_i < vm_count; vm_i++) {
+                  vm_progress = "finished";
+                  
+                  for (k = 0; k < app_count; k++) {
+                    if (progrs_info[vm_i].progress[k].indexOf("Waiting") != -1) {
+                      vm_progress = "waiting";
+                      break;
+                    }
+                  }
+                  
+                  if (vm_progress == "waiting") {
+                    cluster_progress = "waiting";
                   }
                 }
                 
-                if (vm_progress == "waiting") {
-                  cluster_progress = "waiting";
+                html += "<td rowspan='" + (vm_count * app_count) + "' class='hadoop-wizard-" + cluster_progress + "'>"
+                    + "Vcluster ID: " + cid + "<br>Vcluster Name: " +  cname + "</td>";
+                
+                for (vm_i = 0; vm_i < vm_count; vm_i++) {
+                  vm_progress = "finished";
+                  
+                  for (k = 0; k < app_count; k++) {
+                    if (progrs_info[vm_i].progress[k].indexOf("Waiting") != -1) {
+                      vm_progress = "waiting";
+                      break;
+                    }
+                  }
+                  
+                    html += "<td rowspan='" + (app_count) + "' class='hadoop-wizard-" + vm_progress + "'>" + 
+                       + "Vmachine IP: " + progrs_info[vm_i].ip + "<br>Vmachine Name: " + progrs_info[vm_i].node_name + "</td>";
+                  
+                  for (app_i = 0; app_i < app_count; app_i++) {
+                    status = progrs_info[vm_i].progress[app_i];
+                    status1 = "";
+                    if (status[1] == "Waiting") {
+                      status1 = "waiting";
+                    } else if (status[1] == "Finished") {
+                      status1 = "finished";
+                    }
+                    html += "<td class='hadoop-wizard-" + status1 + "'>";
+                    html += status[0];
+                    html += "</td>";
+                    html += "</tr>";
+                  }
                 }
+                
+                
+                html += "</table><p><p>";
               }
-              
-              html += "<td rowspan='" + (vm_count * app_count) + "' class='hadoop-wizard-" + cluster_progress + "'>"
-                  + "Vcluster ID: " + cid + "<br>Vcluster Name: " +  cname + "</td>";
-              
-              for (vm_i = 0; vm_i < vm_count; vm_i++) {
-                vm_progress = "finished";
-                
-                for (k = 0; k < app_count; k++) {
-                  if (progrs_info[vm_i].progress[k].indexOf("Waiting") != -1) {
-                    vm_progress = "waiting";
-                    break;
-                  }
-                }
-                
-                  html += "<td rowspan='" + (app_count) + "' class='hadoop-wizard-" + vm_progress + "'>" + 
-                     + "Vmachine IP: " + progrs_info[vm_i].ip + "<br>Vmachine Name: " + progrs_info[vm_i].node_name + "</td>";
-                
-                for (app_i = 0; app_i < app_count; app_i++) {
-                  status = progrs_info[vm_i].progress[app_i];
-                  status1 = "";
-                  if (status[1] == "Waiting") {
-                    status1 = "waiting";
-                  } else if (status[1] == "Finished") {
-                    status1 = "finished";
-                  }
-                  html += "<td class='hadoop-wizard-" + status1 + "'>";
-                  html += status[0];
-                  html += "</td>";
-                  html += "</tr>";
-                }
-              }
-              
-              
-              html += "</table><p><p>";
+              html += "</center>";
+              my_div.update(html);
             }
-            html += "</center>";
-            my_div.update(html);
+            else {              my_div.update("Error on server");
+            }
+          },
+          failure: function(){
+            my_div.update("Failed to connect server");
           }
-          else {            my_div.update("Error on server");
-          }
-        },
-        failure: function(){
-          my_div.update("Failed to connect server");
-        }
-      });
+        });
         
       }
       
@@ -271,20 +281,17 @@ Ext.override(QoDesk.HadoopWizard, {
       
       progressPane.on("hide", progress_tab_stop_auto_refresh);
       
-      
       tabPane = new Ext.TabPanel({
         activeTab:0,
         items:[create_tab, progressPane]
       });
       
-      
-      
       tabWin = desktop.createWindow({
           autoScroll: true,
-          id: 'hadoop-wizard-win1',
+          id: 'hadoop-wizard-win',
           title: 'Cluster Wizard',
           width:560,
-          height:360,
+          height:430,
           iconCls: 'hadoop-wizard-icon',
           items: tabPane,
           shim:false,
@@ -296,17 +303,12 @@ Ext.override(QoDesk.HadoopWizard, {
       });
       
       tabWin.show();
-      
-      this.tabWin = tabWin;
-      
       tabWin.on("hide", progress_tab_stop_auto_refresh);
-      
-
- 			this.tabWin = tabWin;
+            
+      this.tabWin = tabWin;
     }
     
     tabWin.show();
-
   }  
 });
 
