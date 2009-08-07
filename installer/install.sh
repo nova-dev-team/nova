985bea02bb8447981d77cdf242b73cc7013da52f
root <root@aug14.btc.lib>
2009-08-07 11:39:43 -0400

    Updated installer script

--- installer/install.sh
+++ installer/install.sh
@@ -8,5 +8,7 @@ cd ..
 cd gems
 gem install --no-ri --no-rdoc -l *.gem
 
+rm /usr/bin/rake
+rm /usr/bin/rails
 ln /var/lib/gems/1.8/bin/rake /usr/bin/rake -s
 ln /var/lib/gems/1.8/bin/rails /usr/bin/rails -s
