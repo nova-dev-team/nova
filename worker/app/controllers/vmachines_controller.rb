70fdf782a1caa9773d2f4e3d657785267a37a6c0
Santa Zhang <santa1987@gmail.com>
2009-08-12 16:22:17 +0800

    Adding code for worker, vmachines controller

--- worker/app/controllers/vmachines_controller.rb
+++ worker/app/controllers/vmachines_controller.rb
@@ -1,24 +1,69 @@
 require 'rubygems'
 require 'json'
 require 'libvirt'
+require 'pp'
 
 class VmachinesController < ApplicationController
 
-  # list all vmachines, and show their status
+public
+
+  @@virt_conn = Libvirt::open("qemu:///system")
+
+  # TODO list all vmachines, and show their status
   def index
-
+    doms = @@virt_conn.list_defined_domains
+    render_result doms
   end
 
+  # TODO show detail info of a machine
+  def detail_info
+    begin
+      dom = @@virt_conn.lookup_domain_by_uuid params[:uuid]
+    rescue # would fail if dom not found
+      render_result "Domain not found, check your UUID! (You requested for UUID=#{params[:uuid]})"
+      return
+    end
+    render_result dom
+  end
+
+  # TODO Start an image, non-blocking, and should lock image files
   def start
+    dom = @@virt_conn.lookup_domain_by_uuid params[:uuid]
+    render_result dom
   end
 
+  # TODO
   def stop
+    dom = @@virt_conn.lookup_domain_by_uuid params[:uuid]
+    render_result dom
   end
 
+  # TODO
+  def reboot
+    dom = @@virt_conn.lookup_domain_by_uuid params[:uuid]
+    render_result dom
+  end
+
+  # TODO
   def suspend
+    dom = @@virt_conn.lookup_domain_by_uuid params[:uuid]
+    render_result dom
   end
 
+  # TODO
   def resume
+    dom = @@virt_conn.lookup_domain_by_uuid params[:uuid]
+    render_result dom
+  end
+
+private
+
+  def render_result result
+    respond_to do |accept|
+      accept.json {render :json => result}
+      accept.html {render :text => result.pretty_inspect}
+    end
   end
 
 end
+
