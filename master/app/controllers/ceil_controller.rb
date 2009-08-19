001002692f0bfa5316dde0ee4d8e8acdcf71dc10
root <root@ubun-nfs.(none)>
2009-08-18 22:46:26 -0400

    Nothing

--- master/app/controllers/ceil_controller.rb
+++ master/app/controllers/ceil_controller.rb
@@ -1,2 +1,18 @@
 class CeilController < ApplicationController
+	def retrieve
+
+		respond_to do |accept|
+			accept.json { render :json => request.remote_ip() }
+			accept.html { render :text => request.remote_ip().to_json }
+			
+			#accept.ip { render :text => request.remote_ip() }
+			#accept.config { render :text => request.remote_ip() }
+		end			
+
+	end
+	def report
+				
+
+
+	end
 end
