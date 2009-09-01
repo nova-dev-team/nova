f11d6e97aaa44524544266fe526d07b3f4530743
Santa Zhang <santa1987@gmail.com>
2009-09-01 11:11:45 +0800

    Updated deb list

--- worker/first_run.sh
+++ worker/first_run.sh
@@ -2,6 +2,9 @@
 
 rake backgroundrb:setup
 
+rake db:migrate RAILS_ENV=production
+rake db:migrate RAILS_ENV=development
+
 rake db:migrate:reset RAILS_ENV=production
 rake db:migrate:reset RAILS_ENV=development
 
