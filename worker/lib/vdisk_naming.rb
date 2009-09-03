a842769f8187d64e63f05e5cdb97f394cc9c7eb4
Santa Zhang <santa1987@gmail.com>
2009-09-03 12:56:30 +0800

    Added vdisk_naming.rb & unit tests

---
+++ worker/lib/vdisk_naming.rb
@@ -0,0 +1,45 @@
+module VdiskNaming
+
+  # return a string representing the type of vdisk
+  # could be "iso", "empty", "system", "system.cow", "user", "user.cow"i
+  def vdisk_type filename
+    begin
+      split = filename.split "-"
+      return split[1]
+    rescue
+      raise "'#{filename}' is not in correct form!"
+    end
+  end
+
+  def vdisk_id filename
+    begin
+      split = filename.split "-"
+      return split[0][2..-1].to_i
+    rescue
+      raise "'#{filename}' is not in correct form!"
+    end
+  end
+
+  # return nil in case of failure
+  def vdisk_cow? filename
+    begin
+      split = filename.split "-"
+      return split[1].end_with? "cow"
+    rescue
+      raise "'#{filename}' is not in correct form!"
+    end
+  end
+
+  def vdisk_cow_base filename
+    raise "'#{filename}' is not Copy-On-Write!" unless vdisk_cow? filename
+
+    begin
+      split = filename.split "-"
+      split_again = split[2].split "."
+      return split_again[1].to_i
+    rescue
+      raise "'#{filename}' is not in correct form!"
+    end
+  end
+
+end
