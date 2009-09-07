c74bc9cd2936547efc2d64987e78fab991e7fa95
root <root@ubun-nfs.(none)>
2009-09-07 03:53:51 -0400

    yes we can
    modified ceil conf

--- master/lib/ceil_conf.rb
+++ master/lib/ceil_conf.rb
@@ -13,22 +13,23 @@ CEIL_KEY_SERVER_TYPE = "ftp"
 #client progress id
 CEIL_STATUS = {
 0 => "ceil started",
-10 => "start downloading software package",
+10 => "start downloading binary package",
 15 => "downloading finished, package",
-30 => "start installing software package",
+30 => "start installing binary package",
 35 => "installing finished, package",
 50 => "start downloading key for package",
 55 => "downloading finished, key for package",
 70 => "start dispatching key for package",
 75 => "dispatching finished, key for package",
+80 => "all done, package",
 100 => "ceil exited"
 }
 
 #max app installation id 
 #for calc progress
-CEIL_APP_STATUS_MAX = 75
+CEIL_APP_STATUS_MAX = 80
 #steps to install an app
-CEIL_APP_INSTALL_STEPS = 8   # et. 10 15 30 35 50 55 70 75
+CEIL_APP_INSTALL_STEPS = 9   # et. 10 15 30 35 50 55 70 75 80
 
 module CeilMessage
   def CeilMessage.message(status, category)
