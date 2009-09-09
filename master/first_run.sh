62c599e6f92b98c084ef035b5a2d3a7ee9c4c5b3
Santa Zhang <santa1987@gmail.com>
2009-09-09 21:59:12 +0800

    first_run.rb for master

--- master/first_run.sh
+++ master/first_run.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 
 # triggers 'first_run.sh' script for Ceil component
-
 lib/ceil/server/first_run.sh
 
+rake db:migrate RAILS_ENV=production
+rake db:migrate RAILS_ENV=development
+
+rake db:migrate:reset RAILS_ENV=production
+rake db:migrate:reset RAILS_ENV=development
+
+rake db:fixtures:load RAILS_ENV=production
+rake db:fixtures:load RAILS_ENV=development
