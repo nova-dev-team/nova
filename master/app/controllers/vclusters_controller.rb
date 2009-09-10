7b34dd6a86441b0b1c83cfafbfe8d9aca18efa11
Santa Zhang <santa1987@gmail.com>
2009-09-10 16:13:55 +0800

    Added prototype of 'Add vcluster' dialog

--- master/app/controllers/vclusters_controller.rb
+++ master/app/controllers/vclusters_controller.rb
@@ -1,4 +1,17 @@
 class VclustersController < ApplicationController
 
   before_filter :login_required
+
+  def index
+    result = []
+    Vcluster.all.each do |vcluster|
+      result << {
+        :id => vcluster.id,
+        :name => vcluster.cluster_name
+      }
+    end
+
+    render_data result
+  end
+
 end
