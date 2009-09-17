256a85d3d9bd55051aaee9adb16d19e2ff2b851b
Santa Zhang <santa1987@gmail.com>
2009-09-17 19:45:37 +0800

    Install script requires root.

--- installer/install.sh
+++ installer/install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root!" 1>&2
+	exit 1
+fi
+
 cd debs/archives
 dpkg -i *.deb
 
--- installer/make_installer.sh
+++ installer/make_installer.sh
@@ -4,6 +4,11 @@ LIST_FILE=./debs.list
 DEBS_DIR=debs
 DEBS_DIR_FULL_PATH=$(readlink -f $DEBS_DIR)
 
+if [[ $UID -ne 0 ]]; then
+	echo "$0 must be run as root!"
+	exit 1
+fi
+
 mkdir -p $DEBS_DIR_FULL_PATH/archives/partial
 
 all_debs=( $( cat debs.list ) )
