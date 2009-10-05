83429f71ae55708a55b1bba36f435a5e3608c01a
Santa Zhang <santa1987@gmail.com>
2009-10-05 15:15:29 +0800

    Bug fix

--- worker/lib/image_resource.rb
+++ worker/lib/image_resource.rb
@@ -183,7 +183,7 @@ module ImageResource
     unless File.exist? pooling_name
       copying_lock_fname = "#{pooling_name}.copying"
       copying_lock_file = File.new(copying_lock_fname, "w")
-      File.cp local_filename, pooling_name 
+      FileUtils.cp local_filename, pooling_name 
       copying_lock_file.close
       FileUtils.rm copying_lock_fname
     end
