6fcaa1364ad9b5759c0d0155b887ad2c94fd223e
Santa Zhang <santa1987@gmail.com>
2009-11-12 22:20:59 +0800

    Updated rake jobs

--- scripts/lib/utils.rb
+++ scripts/lib/utils.rb
@@ -1,4 +1,3 @@
-
 def sys_exec cmd
   puts cmd
   IO.popen(cmd) do |f|
@@ -6,3 +5,28 @@ def sys_exec cmd
   end
 end
 
+def all_nodes
+  list = []
+  File.open(File.dirname(__FILE__) + "/../config/node.list", "r") do |f|
+    list = f.readlines
+  end
+  list.each do |line|
+    if line.index "#"
+      line = line[0..(line.index "#") - 1] # skip comments
+    end
+    splt = line.split
+    if splt.length == 2
+      host = splt[0]
+      role = splt[1]
+      yield host, role
+    end
+  end
+end
+
+def all_workers
+  all_nodes do |host, role|
+    if role == "worker"
+      yield host
+    end
+  end
+end
