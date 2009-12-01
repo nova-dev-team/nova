c00899b84f44011ee5a98dc0975a94da2a96ddb1
Santa Zhang <santa1987@gmail.com>
2009-12-01 15:02:03 +0800

    Added xmalloc

--- ndss/xdk/xmemory.c
+++ ndss/xdk/xmemory.c
@@ -1,1 +1,26 @@
+#include <malloc.h>
+
 #include "xmemory.h"
+
+static int _xmalloc_counter = 0;
+
+void* xmalloc(int size) {
+  if (size <= 0)
+    return NULL;
+  else {
+// TODO pthread lock 'counter++' operation
+    _xmalloc_counter++;
+    return malloc(size);
+  }
+}
+
+void xfree(void* ptr) {
+// TODO pthread lock 'counter--' operation
+  _xmalloc_counter--;
+  free(ptr);
+}
+
+int xmem_use() {
+  return _xmalloc_counter;
+}
+
