0af57cae0779f6da2376e84231a735011c24baa9
Santa Zhang <santa1987@gmail.com>
2009-12-02 14:38:45 +0800

    Updated rakefile

--- ndss/app/main.c
+++ ndss/app/main.c
@@ -3,7 +3,10 @@
 #include "ndss.h"
 #include "xdk.h"
 
+#include "main_helper.h"
+
 int main(int argc, char *argv[]) {
   printf("NDSS - Nova Distributed Storage System\n");
+  printf("Testing helper: %d\n", helper_fun());
   return 0;
 }
---
+++ ndss/app/main_helper.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_HELPER_H_
+#define MAIN_HELPER_H_
+
+int helper_fun();
+
+#endif
