acae1b38e0e6e0a4ecab9abc05e7edfd091f31e3
Santa Zhang <santa1987@gmail.com>
2009-12-05 14:08:17 +0800

    Well documented xmemory.h

--- ndss/app/ndss.c
+++ ndss/app/ndss.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-#include "main_helper.h"
+#include "ftp_server.h"
 
 int main(int argc, char *argv[]) {
   printf("NDSS - Nova Distributed Storage System\n");
-  printf("Testing helper: %d\n", helper_fun());
   return 0;
 }
--- ndss/core/ftp.c
+++
@@ -1,1 +0,0 @@
-#include "ftp.h"
