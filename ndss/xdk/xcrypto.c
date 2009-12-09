8a865077c95b05b37cd977ce2ce26277ae027a67
Santa Zhang <santa1987@gmail.com>
2009-12-09 16:12:52 +0800

    Add 3rdparty lib, add xcrypto.h

---
+++ ndss/xdk/xcrypto.c
@@ -0,0 +1,6 @@
+#include "xcrypto.h"
+
+#include "md5.h"
+#include "sha1.h"
+
+
---
+++ ndss/xdk/xcrypto.h
@@ -0,0 +1,17 @@
+#ifndef XCRYPTO_H_
+#define XCRYPTO_H_
+
+/**
+  @author
+    Santa Zhang
+
+  @file
+    xcrypto.h
+
+  @brief
+    A wrapper for 3rd party crypto functions. Currently included SHA-1, MD5
+*/
+
+
+#endif
+
