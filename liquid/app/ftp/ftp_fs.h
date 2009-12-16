6d972e8877d83b0d3611601a8683c4ea57d6abb6
Santa Zhang <santa1987@gmail.com>
2009-12-16 19:41:09 +0800

    Added a dummy ftp_fs (local fs)

--- liquid/app/ftp/ftp_fs.h
+++ liquid/app/ftp/ftp_fs.h
@@ -6,5 +6,7 @@
 
 xsuccess ftp_fs_list_into_xstr(xstr path, xstr holder, xstr error_msg);
 
+xsuccess ftp_fs_try_cwd_cstr(const char* current_dir, const char* new_path, xstr error_msg);
+
 #endif  // LIQUID_FTP_FS_H_
 
