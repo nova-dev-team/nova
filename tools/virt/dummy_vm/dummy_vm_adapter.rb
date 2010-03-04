6af89e4bc5da3ecab9d5efaf8cf14333e1136437
Santa Zhang <santa1987@gmail.com>
2010-03-04 22:05:09 +0800

    Preparing my virt

--- tools/virt/dummy_vm/dummy_vm_adapter.rb
+++ tools/virt/dummy_vm/dummy_vm_adapter.rb
@@ -1,9 +1,9 @@
-# This is an adapter around KVM hypervisor, used by our tool "virt".
+# This is an adapter around dummy_vm hypervisor, used by our tool "virt".
 #
 # Author::  Santa Zhang (mailto:santa1987@gmail.com)
 # Copyright:: Copyright (c) 2010 Nova developmen team
 
-class KvmAdapter
+class DummyVmAdapter
 
   def create_vm params
   end
--- tools/virt/virt.rb
+++ tools/virt/virt.rb
@@ -1,7 +1,11 @@
 require File.join(File.dirname(__FILE__), "kvm/kvm_adapter.rb")
 
-class Virtools
+class Virt
 
+  def initialize hypervisor, working_dir
+    @hypervisor = hypervisor
+    @working_dir = working_dir
+  end
 
 end
 
