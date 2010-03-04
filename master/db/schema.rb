6e46e826b0e5dd252b867330d657b5468da43e51
Santa Zhang <santa1987@gmail.com>
2010-03-04 17:15:57 +0800

    rake nova:master/worker:init

--- master/db/schema.rb
+++ master/db/schema.rb
@@ -11,26 +11,6 @@
 
 ActiveRecord::Schema.define(:version => 20090929110217) do
 
-  create_table "bdrb_job_queues", :force => true do |t|
-    t.text     "args"
-    t.string   "worker_name"
-    t.string   "worker_method"
-    t.string   "job_key"
-    t.integer  "taken"
-    t.integer  "finished"
-    t.integer  "timeout"
-    t.integer  "priority"
-    t.datetime "submitted_at"
-    t.datetime "started_at"
-    t.datetime "finished_at"
-    t.datetime "archived_at"
-    t.string   "tag"
-    t.string   "submitter_info"
-    t.string   "runner_info"
-    t.string   "worker_key"
-    t.datetime "scheduled_at"
-  end
-
   create_table "groups", :force => true do |t|
     t.string   "name",       :limit => 40
     t.boolean  "special",                  :default => false
