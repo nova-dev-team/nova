83552d6a9f47cdd7f769b9585db14cd96ea218ba
Santa Zhang <santa1987@gmail.com>
2010-03-04 17:10:58 +0800

    cleanup backgroundrb from master

--- master/lib/workers/worker_worker.rb
+++
@@ -1,7 +0,0 @@
-class WorkerWorker < BackgrounDRb::MetaWorker
-  set_worker_name :worker_worker
-  def create(args = nil)
-    # this method is called, when worker is loaded for the first time
-  end
-end
-
