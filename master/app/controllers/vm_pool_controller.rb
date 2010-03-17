618e5f16fee7a344492779505409f3b961aaf0cf
Santa Zhang <santa1987@gmail.com>
2010-03-17 21:02:14 +0800

    [scripts/tools] deprecate "scripts" folder, function will be moved into "tools" folder

--- master/app/controllers/vm_pool_controller.rb
+++ master/app/controllers/vm_pool_controller.rb
@@ -1,14 +1,7 @@
-# A special component of Nova, works like a vm_pool.
-# All workers hosts several running vmachines, and when user requested for a new
-# machine, some VM is allocated to the user.
-#
-# Author::    Santa Zhang (mailto:santa1987@gmail.com)
-# Since::     0.3
-
 class VmPoolController < ApplicationController
 
   def index
-    reply_success "TODO"
+    reply_success "TODO: web ui page"
   end
 
   def create
@@ -23,15 +16,15 @@ class VmPoolController < ApplicationController
     reply_success "TODO"
   end
 
-  def request
+  def observe
+    reply_success "TODO"
+  end
+
+  def acquire
     reply_success "TODO"
   end
 
   def release
-    reply_success "TODO"
-  end
-
-  def observe
     reply_success "TODO"
   end
 
