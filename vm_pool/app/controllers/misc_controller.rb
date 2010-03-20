d4024c8907980574cdbdbab721b9561d77cd5184
santazhang <santa1987@gmail.com>
2010-03-20 22:15:16 +0800

    [vm_pool] added port_forwarding to misc_controller

--- vm_pool/app/controllers/misc_controller.rb
+++ vm_pool/app/controllers/misc_controller.rb
@@ -3,6 +3,10 @@
 # Author::    Santa Zhang (mailto:santa1987@gmail.com)
 # Since::     0.3
 
+require 'utils'
+require 'yaml'
+require 'fileutils'
+
 class MiscController < ApplicationController
 
   # Reply the role of this module.
@@ -10,6 +14,81 @@ class MiscController < ApplicationController
   # Since::     0.3
   def role
     reply_success "vm_pool"
+  end
+
+  # Use the 'port_mapper' tool to add a port forwarding.
+  #
+  # Since::     0.3
+  def add_port_mapping
+    return unless valid_ip_and_port_param?
+    fwd_addr = "#{params[:ip]}:#{params["port"]}"
+    # check if already has got a port forwarding
+    port_file = "#{RAILS_ROOT}/log/#{fwd_addr.gsub ":", "_"}.local_port"
+    if File.exists? port_file
+      local_port = File.read(port_file).to_i
+      FileUtils.touch port_file  # mark the file new, so that the daemon will not time out
+    else
+      local_port = 12000 + rand(36000)  # random port
+      if valid_param? params[:timeout] and params[:timeout].to_i.to_s == params[:timeout]
+        timeout = params[:timeout].to_i
+      else
+        timeout = 0 # forever working
+      end
+      my_exec "#{RAILS_ROOT}/lib/port_fwd_daemon #{timeout} #{local_port} #{fwd_addr} #{RAILS_ROOT}/log #{RAILS_ROOT}/../tools/server_side/bin/port_mapper"
+    end
+    reply_success "mapping added", :port => local_port
+  end
+
+  # List all the available port mappings.
+  #
+  # Since::     0.3
+  def list_port_mapping
+    mappings = []
+    Dir.foreach("#{RAILS_ROOT}/log") do |entry|
+      next unless entry =~ /\.pid$/
+      pid = File.read("#{RAILS_ROOT}/log/#{entry}").to_i
+      begin
+        Process.kill 0, pid
+      rescue
+        next
+      end
+      main_fn = entry[0..-5]
+      next unless main_fn =~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+_[0-9]+$/
+      port_fn = File.join "#{RAILS_ROOT}/log/#{main_fn}.local_port"
+      if File.exists? port_fn
+        mappings << {
+          :local_port => File.read(port_fn).to_i,
+          :port => main_fn.split("_")[1].to_i,
+          :ip => main_fn.split("_")[0]
+        }
+      else
+        # kill broken mappings
+        kill_by_pid_file "#{RAILS_ROOT}/log/#{entry}"
+      end
+      
+    end
+    reply_success "query successful", :data => mappings
+  end
+
+  # Remove an existing port mapping.
+  #
+  # Since::     0.3
+  def del_port_mapping
+    return unless valid_ip_and_port_param?
+    fwd_addr = "#{params[:ip]}:#{params["port"]}"
+    port_file = "#{RAILS_ROOT}/log/#{fwd_addr.gsub ":", "_"}.local_port"
+    pid_file = "#{RAILS_ROOT}/log/#{fwd_addr.gsub ":", "_"}.pid"
+
+    if File.exists? pid_file
+      kill_by_pid_file pid_file
+      reply_success "The mapping is deleted."
+    else
+      reply_failure "The port mapping was not found."
+    end
+
+    if File.exists? port_file
+      FileUtils.rm port_file
+    end
   end
 
   # Acquire a VM from the pool.
@@ -26,4 +105,18 @@ class MiscController < ApplicationController
     reply_failure "TODO"
   end
 
+private
+
+  def valid_ip_and_port_param?
+    unless valid_param? params[:ip] and params[:ip].is_ip_addr?
+      reply_failure "Please provide a valid 'ip'."
+      return false
+    end
+    unless valid_param? params[:port] and params[:port].to_i.to_s == params[:port]
+      reply_failure "Please provide a valid 'port'."
+      return false
+    end
+    return true
+  end
+  
 end
