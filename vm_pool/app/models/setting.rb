76d4031af7a593ae43460578677999b070022f11
Santa Zhang <santa1987@gmail.com>
2010-04-08 23:25:53 +0800

    [bug fix] create necessary folders when "rake start/init"

--- vm_pool/app/models/setting.rb
+++ vm_pool/app/models/setting.rb
@@ -5,16 +5,12 @@
 
 class Setting < ActiveRecord::Base
 
-  @@VM_POOL_SIZE = nil
   def Setting.vm_pool_size
-    return @@VM_POOL_SIZE if @@VM_POOL_SIZE
-    @@VM_POOL_SIZE = (Setting.find_by_key "vm_pool_size").value.to_i
+    return (Setting.find_by_key "vm_pool_size").value.to_i
   end
 
-  @@STORAGE_SERVER = nil
   def Setting.storage_server
-    return @@STORAGE_SERVER if @@STORAGE_SERVER
-    @@STORAGE_SERVER = (Setting.find_by_key "storage_server").value
+    return (Setting.find_by_key "storage_server").value
   end
 
 end
