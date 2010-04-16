17cd451fd5436cb155327d657e6e0c9c05ade747
Santa Zhang <santa1987@gmail.com>
2010-04-16 21:53:12 +0800

    [vm_pool] Added Setting.vm_max_use_count

--- worker/app/models/setting.rb
+++ worker/app/models/setting.rb
@@ -103,5 +103,12 @@ LFTP_SCRIPT
     (Setting.find_by_key "storage_server").value
   end
 
+  # Return the max VM use count. If a VM is used more then this count value, it will be destroyed.
+  #
+  # Since::   0.3
+  def Setting.vm_max_use_count
+    (Setting.find_by_key "vm_max_use_count").value
+  end
+
 end
 
