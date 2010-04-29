d1ead2c668fe411fcf634cb8ffcb2cb638850b0d
Santa Zhang <santa1987@gmail.com>
2010-04-29 16:51:40 +0800

    [master] correctly routing to flexui & webui

--- master/app/controllers/flexui_controller.rb
+++ master/app/controllers/flexui_controller.rb
@@ -1,16 +1,25 @@
-# Controller for the Flex ui.
+# The controller for rendering Flex UI.
 #
-# Author::    Santa Zhang (santa1987@gmail.com)
+# Author::    Santa Zhang
 # Since::     0.3
 
 class FlexuiController < ApplicationController
 
   before_filter :login_required
 
-  # Return the Flex container page.
+  # This is a tricky action handler, which redirects to a full url.
+  # This prevents the ambiguity of "/flex" and "/flex/index", and thus avoids the problem in relative path.
   #
   # Since::   0.3
   def index
+    redirect_to "/flexui/ui"
+  end
+
+  # The handler which renders the Flex container.
+  #
+  # Since::   0.3
+  def ui
+    render :file => "#{RAILS_ROOT}/public/flexui/NovaFlexUI.html"
   end
 
 end
--- master/config/routes.rb
+++ master/config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
-  map.home '', :controller => 'webui'
+  map.home '', :controller => 'flexui'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
