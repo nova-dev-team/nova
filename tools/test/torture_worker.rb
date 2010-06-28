d04cb981831df214c52183f1aef6ce53b52194cf
Santa Zhang <santa1987@gmail.com>
2010-06-28 11:35:43 +0800

    Added testing scripts

---
+++ tools/test/torture_worker.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+# This script creates and destroys VMs for many times.
+# Santa Zhang (santa1987@gmail.com)
+
+load File.join File.dirname(__FILE__), "torture_worker.conf"
+
+puts "TODO"
+
