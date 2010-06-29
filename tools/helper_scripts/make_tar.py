0bf11b214fde9bf5b84e6da2d13aeef10410d602
Santa Zhang <santa1987@gmail.com>
2010-06-29 16:01:43 +0800

    Added package maker

---
+++ tools/helper_scripts/make_tar.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# This script creates a tar.gz package in nova source code root, named "nova.tar.gz".
+# Git is required.
+#
+# Author::  Santa Zhang (santa1987@gmail.com)
+# Since::   0.3
+
+import random
+import os
+
+def my_exec(cmd):
+  print "[cmd] %s" % cmd
+  os.system(cmd)
+
+def rand_str(length = 5):
+  str = ""
+  alphabet = "abcdefghijklmnopqrstuvwxyz1234567890"
+  for i in range(length):
+    str += alphabet[random.randint(0, len(alphabet) - 1)]
+  return str
+
+# git clone to a temp dir in "/tmp"
+clone_folder = "/tmp/nova-make-tar." + rand_str() + "/nova"
+source_root = os.getcwd() + os.path.sep + os.path.split(__file__)[0] + "/../../"
+print source_root
+my_exec("git clone %s %s" % (source_root, clone_folder))
+my_exec("rm -Rf %s/.git" % clone_folder)
+
+tar_fn = "%s/nova.tar.gz" % source_root
+my_exec("rm -f %s" % tar_fn)
+my_exec("cd %s && tar pczf %s nova" % (clone_folder + "/..", tar_fn))
+my_exec("rm -Rf %s" % clone_folder)
+
