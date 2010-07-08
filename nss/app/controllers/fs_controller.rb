1b09bff2520d71c39b67e7ddd575df9eae353982
Nova Develop Team <nova-develop@googlegroups.com>
2010-07-08 14:57:48 +0800

    [nss] code updated

---
+++ nss/app/controllers/fs_controller.rb
@@ -0,0 +1,18 @@
+require 'fileutils'
+class FsController < ApplicationController
+  def  listdir
+    if valid_param? params[:dir]
+      dir = params[:dir]
+      list = ""
+       Dir.foreach(dir.to_s) do |entry|
+         list += entry.to_s
+         File.umask()
+       end
+    reply_success "List: #{list}"
+    end
+#   render :text => params[:path] 
+ #   p = params[:dir]
+  #  reply_success "Blah #{p}"
+#render :text => Dir.entries(dir.to_s).join("   ")
+  end
+end
--- nss/config/initializers/session_store.rb
+++ nss/config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_nfs_storage_server_session',
-  :secret      => 'd6a6327901e6dcff8a19ad155eafe15be6b7adfd24b8975a8da9f71c2368f44927ab6731eaa39f5489f98419c52fa63662fd3e2eb11bec8f23324e71e5a7ac6f'
+  :key         => '_nss_session',
+  :secret      => '1faeccebb589ef5f78d647d1577c39e6cd35a2725eafa8f32221a0b2460dc5dde34abb9dface8162b4f1fb96b7890e402093eb7dc7ed4fce94b45968852d35b2'
 }
 
 # Use the database for sessions instead of the cookie-based default,
