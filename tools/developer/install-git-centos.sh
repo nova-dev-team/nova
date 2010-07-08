41dee3ff8ae7df46c44235f96fe832d6bde2156f
root <root@localhost.localdomain>
2010-07-08 15:25:27 +0800

    [developer tools] minor fix in git installer

--- tools/developer/install-git-centos.sh
+++ tools/developer/install-git-centos.sh
@@ -8,9 +8,16 @@ yum makecache
 yum groupinstall "Development Tools"
 yum install gettext-devel expat-devel curl-devel zlib-devel openssl-devel
 
+mkdir -p data
+
 cd data
-wget http://kernel.org/pub/software/scm/git/git-1.7.1.1.tar.gz
-tar zxf git-1.7.1.1.tar.gz
+if [ -e "git-1.7.1.1" ]
+then
+  echo "git-1.7.1.1 already exists, skip downloading"
+else
+  wget http://kernel.org/pub/software/scm/git/git-1.7.1.1.tar.gz
+  tar zxf git-1.7.1.1.tar.gz
+fi
 cd git-1.7.1.1
 
 # configure git to be installed at /usr/bin/git
