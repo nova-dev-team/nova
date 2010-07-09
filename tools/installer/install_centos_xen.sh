ce83788705e4f1df08a5e94402fab790f6c91c5b
root <root@localhost.localdomain>
2010-07-09 23:05:39 +0800

    [installer] updated centos installer

--- tools/installer/install_centos_xen.sh
+++ tools/installer/install_centos_xen.sh
@@ -17,6 +17,7 @@ echo
 echo This script is for research purpose only!
 echo It installs Xen 3.3.1 on CentOS 5.4 from source code.
 echo This script requires Internet connection.
+echo Source code will be compiled at '/tmp/nova_build'
 echo
 read -p "Press ENTER to start installation, or press Ctrl+C to quit..."
 clear
@@ -60,29 +61,56 @@ if grep -q "CentOS" "/etc/issue" ; then
   echo ===========================
   echo Phase 3: Unpack source code
   echo ===========================
+  mkdir -p /tmp/nova_build
   cd $SCRIPT_ROOT/data/src
-  cp xen-3.3.1.tar.gz /tmp
-  cd /tmp
+  cp xen-3.3.1.tar.gz /tmp/nova_build
+  cd /tmp/nova_build
   tar xzf xen-3.3.1.tar.gz
+
   # get back to where I were
   cd $RUNNING_ROOT
-  cd $SRCIPT_ROOT/data/src
+
+  cd $SCRIPT_ROOT/data/src
   rpm -i kernel-2.6.18-164.15.1.el5.src.rpm
   cd /usr/src/redhat
   rpmbuild -bp --with xenonly SPECS/kernel-2.6.spec
-  mkdir /tmp/kernel_src
-  cp -r BUILD/kernel-2.6.18/linux-2.6.18.x86_64 /tmp/kernel_src
+  cp -r BUILD/kernel-2.6.18/linux-2.6.18.x86_64 /tmp/nova_build
 
   echo ================
   echo Phase 4: Compile
   echo ================
   
-  cd /tmp/xen-3.3.1
+  cd /tmp/nova_build/xen-3.3.1
   make xen tools
-  cd /tmp/kernel_src
+  cd /tmp/nova_build/linux-2.6.18.x86_64
   cp configs/kernel-2.6.18-x86_64-xen.config .config
   make
 
+  echo ================
+  echo Phase 5: Install
+  echo ================
+  cd /tmp/nova_build/xen-3.3.1
+  make install-xen install-tools
+  cd /tmp/nova_build/linux-2.6.18.x86_64
+  make modules_install install
+  mkinitrd /boot/initrd-2.6.18-prep.img 2.6.18-prep
+
+  echo ====================================================================
+  echo
+  echo *** Finished installing Xen and Linux kernel.
+  echo
+  echo The compiled binary is in '/tmp/nova_build' directory.
+  echo Now, you need to config the Grub loader.
+  echo Here is an example Grub config file:
+  echo "(sda1 as boot partition, sda2 as main partition)"
+  echo 
+  echo title Xen
+  echo "        root(hd0,0)"
+  echo "        kernel /xen-3.3.1.gz"
+  echo "        module /vmlinuz-2.5.18-prep ro root=/dev/sda2"
+  echo "        module /initrd-2.6.18-prep.img"
+  echo 
+  echo ====================================================================
 
   # get back to where I were
   cd $RUNNING_ROOT
