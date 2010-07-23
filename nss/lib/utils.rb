03ebef6dca54eb4586f90552d8aee1645eb44b03
root <root@RH147.(none)>
2010-07-23 23:05:24 +0800

    [nss] several fixes

---
+++ nss/lib/utils.rb
@@ -0,0 +1,7 @@
+# This file provides basic utility for worker module.
+#
+# Author::    Santa Zhang (santa1987@gmail.com)
+# Since::     0.3
+
+require "#{File.dirname __FILE__}/../../common/lib/utils.rb"
+
