ab0b7cceb39b705395355d1a3c6bafa196dc7a97
Santa Zhang <santa1987@gmail.com>
2010-07-28 19:40:41 +0800

    [master] updated perf_log controller, added time & pm_ip optional args

--- master/app/controllers/perf_log_controller.rb
+++ master/app/controllers/perf_log_controller.rb
@@ -1,5 +1,52 @@
+# Performance log controller.
+#
+# Since::   0.3
+
 class PerfLogController < ApplicationController
+
+  # Reply an array of perf logs.
+  # Optional args:
+  # * time: from when should the perf log be retrieved
+  # * pm_ip: the pmachine ip, of which the perf log will be retireved
+  #
+  # Since::   0.3
   def show
-    reply_success "Query successful!", :data => PerfLog.find_all_by_pmachine_id(params[:pm_id])
+    start_time = nil
+    start_time = params[:time] if valid_param? params[:time]
+    pm_id = nil
+    if valid_param? params[:pm_ip]
+      pm = Pmachine.find_by_ip params[:pm_ip]
+      if pm != nil
+        pm_id = pm.id
+      end
+    end
+
+    if start_time != nil and pm_id != nil
+      perf_data = PerfLog.find(:all, :conditions => ["time >= ? and pmachine_id = ?",start_time, pm_id])
+    elsif start_time != nil and pm_id == nil
+      perf_data = PerfLog.find(:all, :conditions => ["time >= ?", start_time])
+    elsif start_time == nil and pm_id != nil
+      perf_data = PerfLog.find(:all, :conditions => ["pmachine_id = ?", pm_id])
+    else
+      # start_time == nil and pm_id == nil
+      perf_data = PerfLog.all
+    end
+
+    reply_data = []
+    perf_data.each do |perf|
+      reply_data << {
+        :dAvail => perf.dAvail,
+        :time => perf.time,
+        :memTotal => perf.memTotal,
+        :pmachine_id => perf.pmachine_id,
+        :memFree => perf.memFree,
+        :CPU => perf.CPU,
+        :dSize => perf.dSize,
+        :Tran => perf.Tran,
+        :Rece => perf.Rece
+      }
+    end
+
+    reply_success "Query successful!", :start_time => start_time, :pm_id => pm_id, :pm_ip => params[:pm_ip], :data => reply_data
   end
 end
