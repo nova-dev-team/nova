ac7d67f9e41f3353a2e9ba8c359d27a04dc5e588
Santa Zhang <santa1987@gmail.com>
2010-09-02 23:16:22 +0800

    [tools] liquid update

---
+++ tools/server_side/liquid/app/ftp/ftp_acl.c
@@ -0,0 +1,2 @@
+#include "ftp_acl.h"
+
---
+++ tools/server_side/liquid/app/ftp/ftp_acl.h
@@ -0,0 +1,16 @@
+#ifndef LIQUID_FTP_ACL_H_
+#define LIQUID_FTP_ACL_H_
+
+/**
+ * @author
+ *  Santa Zhang
+ *
+ * @file
+ *  ftp_acl.h
+ *
+ * @brief
+ *  Provide user authentication and ACL protection for filesystem.
+ */
+
+#endif  // LIQUID_FTP_ACL_H_
+
