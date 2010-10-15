568f17a5cc3305e18486ad56e4742dca8720eecf
Santa Zhang <santa1987@gmail.com>
2010-10-15 18:52:59 +0800

    [auto] update nova/tools/server_side/xdk/xnet.h to newest version

--- tools/server_side/xdk/xnet.h
+++ tools/server_side/xdk/xnet.h
@@ -69,7 +69,7 @@ const char* xsocket_get_host_cstr(xsocket xs);
 /**
   @brief
     Get host machine's service port.
-
+  
   @param xs
     The xsocket object containing the port number.
 
@@ -170,8 +170,10 @@ void xsocket_delete(xsocket xs);
     One of the xsockets.
   @param xs2
     One of the xsockets.
+  @param sleep_usec
+    The sleeping interval, in useconds (10e-6 second).
 */
-void xsocket_shortcut(xsocket xs1, xsocket xs2);
+void xsocket_shortcut(xsocket xs1, xsocket xs2, int sleep_usec);
 
 struct xserver_impl;
 
@@ -233,18 +235,6 @@ xserver xserver_new(xstr host, int port, int backlog, xserver_acceptor acceptor,
     Remember, xserver will be self-destroyed after this call!
 */
 xsuccess xserver_serve(xserver xs);
-
-/**
- * @brief
- *  Destroy an xserver.
- *
- * It could only be used when xserver_serve() is not called.
- * When xserver_server() finished, it will automatically call xserver_delete().
- *
- * @param xs
- *  The xserver to be destroyed.
- */
-void xserver_delete(xserver xs);
 
 /**
   @brief
