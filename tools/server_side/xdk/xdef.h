60adea97e37df6319f0ca7b30748e7a26e2cf6e8
Santa Zhang <santa1987@gmail.com>
2010-10-15 18:52:58 +0800

    [auto] update nova/tools/server_side/xdk/xdef.h to newest version

--- tools/server_side/xdk/xdef.h
+++ tools/server_side/xdk/xdef.h
@@ -32,49 +32,9 @@ typedef enum xsuccess {
 } xsuccess;
 
 /**
- *  @brief
- *    The byte type, used for storing binary data.
- */
-typedef unsigned char xbyte;
-
-/**
   @brief
     When value range is non-negative, -1 could be used to act as a special mark for "unlimited" large value.
 */
 #define XUNLIMITED -1
-
-/**
- *  @brief
- *    Indicates the param is used for output (using pointers).
- */
-#define XOUT
-
-/**
- *  @brief
- *    Indicates the param is used for input.
- */
-#define XIN
-
-/**
- *  @brief
- *    Indicates the param is used both for input and output (using pointers).
- */
-#define XINOUT
-
-/**
- * @brief
- *  Function for comparing 2 x-objects.
- *
- * @param obj1
- *  x-object 1
- * @param obj2
- *  x-object 2
- *
- * @return
- *  If obj1 > obj2, return 1;
- *  If obj1 == obj2, return 0;
- *  If obj1 < obj2, return -1.
- */
-typedef int (*xcompare_f)(void* obj1, void* obj2);
 
 #endif  // XDK_XDEF_H_
