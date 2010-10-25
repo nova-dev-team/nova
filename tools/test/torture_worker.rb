64ed89119e9c359ba391b3a73975233749d3e1bc
Santa Zhang <santa1987@gmail.com>
2010-10-25 16:23:01 +0800

    [tools] cleanup tools folder

--- tools/test/torture_worker.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby
-
-# This script creates and destroys VMs for many times.
-# Santa Zhang (santa1987@gmail.com)
-
-load File.join File.dirname(__FILE__), "torture_worker.conf"
-
-puts "TODO"
-
