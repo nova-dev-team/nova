9d319b85085cd3f6bfbbddbba956fd8bba5c64e9
Santa Zhang <santa1987@gmail.com>
2010-10-27 16:19:22 +0800

    [installer] could infer rubygems package dependency

--- tools/installer/make_installer.py
+++ tools/installer/make_installer.py
@@ -2,9 +2,43 @@
 
 import urllib2
 
+def version_to_array(ver_text):
+  ver_arr = []
+  splt_arr = []
+  seg = ""
+  mode = None
+  for c in ver_text:
+    if c.isdigit():
+      if mode == "number":
+        seg += c
+      else:
+        splt_arr += seg,
+        seg = c
+        mode = "number"
+    else:
+      if mode == "number":
+        splt_arr += seg,
+        seg = c
+        mode = "text"
+      else:
+        seg += c
+  splt_arr += seg,
+  for seg in splt_arr:
+    if len(seg) == 0:
+      continue
+    elif seg.isdigit():
+      ver_arr += int(seg),
+    else:
+      ver_arr += seg,
+  return ver_arr
+
 def list_rubygems_org_versions(gem_name):
   page_url = "http://rubygems.org/gems/%s/versions" % gem_name
-  html_text = urllib2.urlopen(page_url).read()
+  html_text = ""
+  try:
+    html_text = urllib2.urlopen(page_url).read()
+  except:
+    print "[exception] %s" % page_url
 
   # parse and find the versions
   ver_list = []
@@ -21,7 +55,6 @@ def list_rubygems_org_versions(gem_name):
     version_text = search_text[idx:idx2]
     ver_list += version_text,
     idx = idx2
-
   return ver_list
 
 def parse_rubygems_org_page(gem_name, gem_version = None, mode = "development"):
@@ -30,7 +63,11 @@ def parse_rubygems_org_page(gem_name, gem_version = None, mode = "development"):
     page_url = "http://rubygems.org/gems/%s/versions/%s" % (gem_name, gem_version)
   else:
     page_url = "http://rubygems.org/gems/%s" % gem_name
-  html_text = urllib2.urlopen(page_url).read()
+  html_text = ""
+  try:
+    html_text = urllib2.urlopen(page_url).read()
+  except:
+    print "[exception] %s" % page_url
 
   # parse download location
   idx = html_text.index("<div class=\"admin\">")
@@ -63,18 +100,56 @@ def parse_rubygems_org_page(gem_name, gem_version = None, mode = "development"):
     _parse_dependency("development")
   return info_hash
 
-def rubygems_infer_dependency(ver_info, mode = "development"):
+def rubygems_infer_dependency(ver_info, mode = "development", visited_list = []):
   gem_name, dep_op, gem_version = ver_info.split()
-  if dep_op == "=":
-    info = parse_rubygems_org_page(gem_name, gem_version, mode)
-    print info
-  elif dep_op == ">=" or dep_op == "~>":
+  if dep_op == ">=" or dep_op == "~>":
     ver_list = list_rubygems_org_versions(gem_name)
-    print ver_list
+    target_ver_arr = version_to_array(gem_version)
+    usable_ver_list = []
+    for ver in ver_list:
+      ver_arr = version_to_array(ver)
+      is_stable_release = True
+      for c in ver:
+        if c == "." or c.isdigit():
+          pass
+        else:
+          is_stable_release = False
+          break
+      if ver_arr >= target_ver_arr and is_stable_release == True:
+        usable_ver_list += ver,
+    chosen_version = None
+    if dep_op == ">=":
+      gem_version = usable_ver_list[0]
+    elif dep_op == "~>":
+      gem_version = usable_ver_list[-1]
+
+
+  def _package_visited(package):
+    pkg_name = package.split()[0]
+    for visited in visited_list:
+      if visited.startswith(pkg_name):
+        return True
+    return False
+
+  print gem_name, gem_version
+  visited_list += "%s %s %s" % (gem_name, dep_op, gem_version),
+  info = parse_rubygems_org_page(gem_name, gem_version, mode)
+#  print "runtime dependency:",
+#  for dep in info["runtime_dependency"]:
+#    print dep,
+#  print
+  for dep in info["runtime_dependency"]:
+    if not _package_visited(dep):
+      rubygems_infer_dependency(dep, mode, visited_list)
+  if mode == "development":
+#    print "development dependency:",
+#    for dep in info["development_dependency"]:
+#      print dep,
+#    print
+    for dep in info["development_dependency"]:
+      if not _package_visited(dep):
+        rubygems_infer_dependency(dep, mode, visited_list)
 
 if __name__ == "__main__":
-  rubygems_infer_dependency("rails = 3.0.1")
-  rubygems_infer_dependency("warden = 1.0.1")
-  rubygems_infer_dependency("devise = 1.1.3")
-  rubygems_infer_dependency("bundler ~> 1.0.0")
+  rubygems_infer_dependency("rails >= 3.0.0")
 
