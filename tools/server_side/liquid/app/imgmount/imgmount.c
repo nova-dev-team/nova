8718150fc7ef389453b45d68078de5715e3e7191
Santa Zhang <santa1987@gmail.com>
2010-11-05 16:57:06 +0800

    [auto] update nova/tools/server_side/liquid/app/imgmount/imgmount.c to newest version

--- tools/server_side/liquid/app/imgmount/imgmount.c
+++ tools/server_side/liquid/app/imgmount/imgmount.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "xutils.h"
 #include "xoption.h"
 #include "imgmount.h"
 #include "imgmount_instance.h"
@@ -13,37 +12,11 @@ void imgmount_print_help() {
   printf("\n");
   printf("The <mount_home> will have the following content:\n");
   printf("  cache/        cached files\n");
+  printf("  cntl/         controlling utilities\n");
   printf("  data/         where your data will be located\n");
-  printf("  tmp/          temporary files\n");
   printf("  log/          application log data\n");
+  printf("  tmp/          temporary files\n");
   printf("\n");
-}
-
-static xsuccess imgmount_main_real(int argc, char* argv[]) {
-  xsuccess ret = XFAILURE;
-  char* ip_port = argv[2];
-  xstr server_ip = xstr_new();
-  int server_port = -1;
-  xstr mount_point = xstr_new_from_cstr(argv[3]);
-
-  if (xinet_split_host_port(ip_port, server_ip, &server_port) == XSUCCESS) {
-    imgmount_instance inst = imgmount_instance_new(server_ip, server_port, mount_point);
-    if (inst != NULL) {
-      ret = imgmount_instance_run(inst);
-      imgmount_instance_delete(inst);
-    } else {
-      ret = XFAILURE;
-      fprintf(stderr, "Failed to boot imgmount!\n");
-    }
-    // server_ip and mount_point is managed by imgmount_instance, no need to delete them
-    
-  } else {
-    fprintf(stderr, "incorrect ip and port info!\n");
-    ret = XFAILURE;
-    xstr_delete(server_ip);
-    xstr_delete(mount_point);
-  }
-  return ret;
 }
 
 xsuccess imgmount_main(int argc, char* argv[]) {
@@ -54,7 +27,7 @@ xsuccess imgmount_main(int argc, char* argv[]) {
   if (xoption_has(xopt, "help") || xoption_has(xopt, "h") || argc < 4) {
     imgmount_print_help();
   } else {
-    ret = imgmount_main_real(argc, argv);
+    ret = imgmount_instance_run(argc, argv);
   }
   xoption_delete(xopt);
   return ret;
