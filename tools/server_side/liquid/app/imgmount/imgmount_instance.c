a6cca3020a8046f2711a08997ec6fec98b94afa5
Santa Zhang <santa1987@gmail.com>
2010-11-05 16:57:06 +0800

    [auto] update nova/tools/server_side/liquid/app/imgmount/imgmount_instance.c to newest version

--- tools/server_side/liquid/app/imgmount/imgmount_instance.c
+++ tools/server_side/liquid/app/imgmount/imgmount_instance.c
@@ -1,21 +1,39 @@
 #include "xnet.h"
 #include "xmemory.h"
 #include "xutils.h"
+#include "xlog.h"
 
 #include "imgmount_instance.h"
-#include "imgmount_ls.h"
-#include "imgmount_conn.h"
+#include "imgmount_fuse.h"
+#include "imgmount_fs_cache.h"
 
+/**
+  @brief
+    The imgmount instance.
+*/
 struct imgmount_instance_impl {
-  xstr imgdir_ip;
-  int imgdir_port;
-  xstr mount_home;
+  xsocket imgdir_sock;  ///<  @brief Connection to imgdir.
+  xstr mount_home;  ///<  @brief The mount home, where the imgmount filesystem is organized.
+  fs_cache fs_root;  ///<  @brief The cached root of imgdir filesystem.
 };
+
+xstr get_mount_home(imgmount_instance inst) {
+  return inst->mount_home;
+}
+
+xsocket get_imgdir_sock(imgmount_instance inst) {
+  return inst->imgdir_sock;
+}
+
+fs_cache get_root(imgmount_instance inst) {
+  return inst->fs_root;
+}
+
 
 // check existance of the mount_home, and add contents to it
 static xsuccess prepare_mount_home(const char* mount_home_cstr) {
   xsuccess ret = XSUCCESS;
-  char* sub_dirs[] = {"cache", "data", "tmp", NULL};
+  char* sub_dirs[] = {"cache", "cntl", "data", "log", "tmp", NULL};
   int i = 0;
   xstr sub_dir_path = xstr_new();
   while (sub_dirs[i] != NULL && ret == XSUCCESS) {
@@ -27,11 +45,20 @@ static xsuccess prepare_mount_home(const char* mount_home_cstr) {
   return ret;
 }
 
-imgmount_instance imgmount_instance_new(xstr server_ip, int server_port, xstr mount_home) {
+
+static void imgmount_instance_delete(imgmount_instance inst) {
+  xsocket_delete(inst->imgdir_sock);
+  xstr_delete(inst->mount_home);
+  fs_cache_delete(inst->fs_root);
+  xfree(inst);
+}
+
+
+static imgmount_instance imgmount_instance_new(xstr server_ip, int server_port, xstr mount_home) {
   imgmount_instance inst = xmalloc_ty(1, struct imgmount_instance_impl);
-  inst->imgdir_ip = server_ip;
-  inst->imgdir_port = server_port;
+  inst->imgdir_sock = xsocket_new(server_ip, server_port);
   inst->mount_home = mount_home;
+  inst->fs_root = fs_cache_new_root();
   if (prepare_mount_home(xstr_get_cstr(mount_home)) == XFAILURE) {
     // failed to prepare mount home, destroy the instance and cancel all operations,
     // then set return value to NULL, indicate a failed operation
@@ -42,24 +69,83 @@ imgmount_instance imgmount_instance_new(xstr server_ip, int server_port, xstr mo
 }
 
 // helper function to establish connection to imgdir server
-static xsuccess imgmount_instance_connect(imgmount_instance inst) {
+static xsuccess imgmount_instance_connect_imgdir(imgmount_instance inst) {
   xsuccess ret = XFAILURE;
-  // TODO
+  ret = xsocket_connect(inst->imgdir_sock);
+  if (ret == XSUCCESS) {
+    // do imgdir handshake
+    ret = xsocket_write_line(inst->imgdir_sock, "accept_me\r\n");
+    if (ret == XSUCCESS) {
+      xstr reply_text = xstr_new();
+      if (xsocket_read_line(inst->imgdir_sock, reply_text) == XFAILURE || xstr_eql_cstr(reply_text, "accepted") == XFALSE) {
+        ret = XFAILURE;
+      }
+      xstr_delete(reply_text);
+    }
+    // handshake done
+  }
   return ret;
 }
 
-xsuccess imgmount_instance_run(imgmount_instance inst) {
+static struct fuse_operations imgmount_filesystem_operations = {
+  .getattr = imgmount_getattr,
+  .open    = imgmount_open,
+  .read    = imgmount_read,
+  .readdir = imgmount_readdir,
+};
+
+// helper function for handling real running routine in imgmount_instance_run()
+static xsuccess instance_real_run(imgmount_instance inst, int argc, char* argv[]) {
   xsuccess ret;
-  
-  ret = imgmount_instance_connect(inst);
-  // TODO
+  ret = imgmount_instance_connect_imgdir(inst);
+  if (ret == XFAILURE) {
+    xlog_fatal("cannot connect to imgdir server!\n");
+  } else {
+    int fake_argc = argc - 2, i;
+    char** fake_argv = xmalloc_ty(fake_argc, char *);
+    xstr sub_dir_path = xstr_new();
+
+    // fake argv: liquid_app, mount_data_dir, [optinal_args]
+    fake_argv[0] = argv[0];
+    xjoin_path_cstr(sub_dir_path, xstr_get_cstr(inst->mount_home), "data");
+    fake_argv[1] = (char *) xstr_get_cstr(sub_dir_path);
+
+    // fill in optional FUSE args into argv
+    for (i = 2; i + 2 < argc; i++)
+      fake_argv[i] = argv[i + 2];
+
+    // leave operations to fuse
+    fuse_main(fake_argc, fake_argv, &imgmount_filesystem_operations, inst);
+    xstr_delete(sub_dir_path);
+    xfree(fake_argv);
+  }
   return ret;
 }
 
-void imgmount_instance_delete(imgmount_instance inst) {
-  xstr_delete(inst->imgdir_ip);
-  inst->imgdir_ip = NULL;
-  xstr_delete(inst->mount_home);
-  inst->mount_home = NULL;
-  xfree(inst);
+xsuccess imgmount_instance_run(int argc, char* argv[]) {
+  xsuccess ret;
+  const char* ip_port = argv[2];
+  xstr server_host = xstr_new();
+  int server_port = -1;
+  xstr mount_home = xstr_new_from_cstr(argv[3]);
+
+  if (xinet_split_host_port(ip_port, server_host, &server_port) == XSUCCESS) {
+    imgmount_instance inst = imgmount_instance_new(server_host, server_port, mount_home);
+    if (inst != NULL) {
+      // call real running handling routine
+      ret = instance_real_run(inst, argc, argv);
+      imgmount_instance_delete(inst);
+    } else {
+      ret = XFAILURE;
+      fprintf(stderr, "Failed to boot imgmount!\n");
+    }
+    // server_host and mount_home is managed by imgmount_instance, no need to delete them
+  } else {
+    fprintf(stderr, "incorrect ip and port info!\n");
+    ret = XFAILURE;
+    xstr_delete(server_host);
+    xstr_delete(mount_home);
+  }
+  return ret;
 }
+
