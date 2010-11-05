a66cd1d7df378616c8786b0e8e0de57843e9c1b2
Santa Zhang <santa1987@gmail.com>
2010-11-05 16:57:06 +0800

    [auto] update nova/tools/server_side/liquid/app/imgdir/imgdir_session.h to newest version

--- tools/server_side/liquid/app/imgdir/imgdir_session.h
+++ tools/server_side/liquid/app/imgdir/imgdir_session.h
@@ -12,4 +12,39 @@
     Abstraction for a client connection.
 */
 
+#include "imgdir_server.h"
+
+// the hidden implementation of imgdir_session
+struct imgdir_session_impl;
+
+/**
+  @brief
+    Interface of imgdir_session.
+*/
+typedef struct imgdir_session_impl* imgdir_session;
+
+/**
+  @brief
+    Create a new imgdir_session.
+
+  @param client_sock
+    Client socket connection.
+  @param svr
+    Handle of server model, contains global info.
+
+  @return
+    The newly created connection session.
+*/
+imgdir_session imgdir_session_new(xsocket client_sock, imgdir_server svr);
+
+/**
+  @brief
+    Start serving a client session.
+    The session it self will be destroyed after service is over.
+
+  @param session
+    The imgdir connection session.
+*/
+void imgdir_session_serve(imgdir_session session);
+
 #endif  // IMGDIR_SESSION_H_
