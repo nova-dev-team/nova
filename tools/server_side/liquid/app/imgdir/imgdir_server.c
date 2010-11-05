3f306af952638f848d7c8f0d5842fedfd82561cd
Santa Zhang <santa1987@gmail.com>
2010-11-05 16:57:05 +0800

    [auto] update nova/tools/server_side/liquid/app/imgdir/imgdir_server.c to newest version

--- tools/server_side/liquid/app/imgdir/imgdir_server.c
+++ tools/server_side/liquid/app/imgdir/imgdir_server.c
@@ -10,37 +10,61 @@
 
 #include "imgdir_server.h"
 #include "imgdir_session.h"
-#include "imgdir_ls.h"
 
 void imgdir_print_help() {
   printf("This shall be done!\n");
 }
 
+static void imgdir_server_delete(imgdir_server svr) {
+  if (svr->root != NULL) {
+    fs_cache_delete(svr->root);
+  }
+  if (svr->fs != NULL) {
+    fsdb_close(svr->fs);
+  }
+  xfree(svr);
+}
+
+static imgdir_server imgdir_server_new(const char* fsdb_fn) {
+  imgdir_server svr = xmalloc_ty(1, struct imgdir_server_impl);
+  svr->root = fsdb_load_root(fsdb_fn, &(svr->fs));
+  if (svr->root == NULL || svr->fs == NULL) {
+    // failed to create server model, delete it now!
+    imgdir_server_delete(svr);
+    svr = NULL;
+  }
+  return svr;
+}
+
 static void cmd_acceptor(xsocket client_xs, void* args) {
-  int buf_size = 8192;
-  char* inbuf = xmalloc_ty(buf_size, char);
-  char* outbuf = xmalloc_ty(buf_size, char);
-
-  xlog_info("[imgdir] client connected\n");
-
-  xfree(inbuf);
-  xfree(outbuf);
+  imgdir_server svr = (imgdir_server) args;
+  imgdir_session session = imgdir_session_new(client_xs, svr);
+  xlog_info("[imgdir] new client connected\n");
+  imgdir_session_serve(session);
 }
 
 static xsuccess imgdir_service(xstr host, int port, int backlog) {
   xsuccess ret;
-  void* args = host;
   char serv_mode = 't'; // serv in new thread
-  xserver xs = xserver_new(host, port, backlog, cmd_acceptor, XUNLIMITED, serv_mode, args);
-
-  if (xs == NULL) {
-    xlog_fatal("[imgdir] in imgstore_service(): failed to init xserver!\n");
+  char* fsdb_fn = "fsdb.sqlite3"; // TODO parse from command line
+  imgdir_server svr = imgdir_server_new(fsdb_fn);
+  if (svr == NULL) {
     ret = XFAILURE;
+    xlog_fatal("[imgdir] failed to create server model!\n");
+    // release "host", since it is not managed by xserver yet
+    xstr_delete(host);
   } else {
-    xlog_info("[imgdir] imgstore server started on %s:%d\n", xstr_get_cstr(host), port);
-    ret = xserver_serve(xs);  // xserver is self destrying, after service, it will destroy it self
+    xserver xs = xserver_new(host, port, backlog, cmd_acceptor, XUNLIMITED, serv_mode, svr);
+    if (xs == NULL) {
+      xlog_fatal("[imgdir] in imgstore_service(): failed to init xserver!\n");
+      ret = XFAILURE;
+    } else {
+      xlog_info("[imgdir] imgstore server started on %s:%d\n", xstr_get_cstr(host), port);
+      ret = xserver_serve(xs);  // xserver is self destrying, after service, it will destroy it self
+    }
+    imgdir_server_delete(svr);
+    // don't need to release "host", since it is managed by xservers
   }
-  // don't need to release "host", since it is managed by xserver
   return ret;
 }
 
