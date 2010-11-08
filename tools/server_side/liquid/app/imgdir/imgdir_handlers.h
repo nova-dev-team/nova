a110328dd13880c5b1bfd51c259f6ed9818dd701
Santa Zhang <santa1987@gmail.com>
2010-11-08 18:52:27 +0800

    [auto] update nova/tools/server_side/liquid/app/imgdir/imgdir_handlers.h to newest version

--- tools/server_side/liquid/app/imgdir/imgdir_handlers.h
+++ tools/server_side/liquid/app/imgdir/imgdir_handlers.h
@@ -16,7 +16,23 @@
 #include "xnet.h"
 #include "imgdir_session.h"
 
+/**
+  @brief
+    Handle list requests.
+
+  If
+
+  @param session
+    The client session.
+  @param req_head
+    The client's request head.
+
+  @return
+    Whether the request has been successfully handled.
+*/
 xsuccess imgdir_handle_ls(XIN imgdir_session session, xstr req_head);
+
+xsuccess imgdir_handle_mkdir(XIN imgdir_session session, xstr req_head);
 
 #endif  // LIQUID_IMGDIR_HANDLERS_H_
 
