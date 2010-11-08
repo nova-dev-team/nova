0ca57e220ff0579732381852403a4b2c9e1ced3e
Santa Zhang <santa1987@gmail.com>
2010-11-08 18:52:28 +0800

    [auto] update nova/tools/server_side/liquid/app/imgmount/imgmount_fuse.h to newest version

--- tools/server_side/liquid/app/imgmount/imgmount_fuse.h
+++ tools/server_side/liquid/app/imgmount/imgmount_fuse.h
@@ -14,6 +14,8 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/statvfs.h>
+#include <sys/types.h>
 
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
@@ -36,7 +38,55 @@ int imgmount_open(const char *path, struct fuse_file_info *fi);
 
 int imgmount_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
 
+/**
+  @brief
+    Read dir contents.
+
+  @param path
+    Absolute path of the dir entry.
+  @param buf
+    Data to be returned to FUSE.
+  @param filler
+    Utility provided by FUSE to fill dir entry data into buf.
+  @param offset
+    Parameter provided by FUSE.
+  @param fi
+    Parameter provided by FUSE.
+
+  @return
+    Return 0 if no error. Else return corresponding -ERRNO.
+*/
 int imgmount_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi);
+
+/**
+  @brief
+    Stat filesystem status.
+
+  @param path
+    The path where the filesystem stat is called.
+  @param vfs
+    The statvfs structure to hold data.
+
+  @return
+    Return 0 if no error. Else return corresponding -ERRNO.
+*/
+int imgmount_statfs(const char *path, struct statvfs *vfs);
+
+/**
+  @brief
+    Check access permission.
+
+  @param path
+    The path which should be checked.
+  @param mode
+    The access mode.
+
+  @return
+    Return 0 if no error. Else return corresponding -ERRNO.
+*/
+int imgmount_access(const char* path, int mode);
+
+int imgmount_mkdir(const char* path, mode_t mode);
 
 #endif  // IMGMOUNT_FUSE_H_
 
