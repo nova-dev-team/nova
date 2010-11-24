751cf7dda4bc30b4542221e280c38cfda507db65
Santa Zhang <santa1987@gmail.com>
2010-11-24 21:10:46 +0800

    [master] added mac_address for workers

--- master/db/migrate/20090627122350_create_pmachines.rb
+++ master/db/migrate/20090627122350_create_pmachines.rb
@@ -15,6 +15,9 @@ class CreatePmachines < ActiveRecord::Migration
       # The uuid of the worker machine
       t.column :uuid,           :string
 
+      # The MAC address of the worker machine, used for remote booting
+      t.column :mac_address,    :string
+
       # The limit of running VMs on this machine.
       # It is not a hard limit, but creating VMs more than this limit will result in low performance.
       t.column :vm_capacity,    :integer, :default => 2
