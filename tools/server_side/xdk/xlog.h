a65f73a0dd9fe5fac2e5dd36442658d2e0fbeb62
Santa Zhang <santa1987@gmail.com>
2010-12-20 22:16:17 +0800

    [auto] update nova/tools/server_side/xdk/xlog.h to newest version

--- tools/server_side/xdk/xlog.h
+++ tools/server_side/xdk/xlog.h
@@ -15,85 +15,160 @@
 
   @brief
     Provide logging utility.
-
-    The logging system uses the following parameters:<br>
-    --log-level=2<br>
-    Loging levels are: 0 (fatal), 1 (error), 2 (warning), 3 (info), 4 (debug), 5~7(user defined).<br>
-
-    --log-dir=.<br>
-    Where all the logs and log history will be saved.<br>
-
-    --log-maxsize=10M<br>
-    Max file size of the log, if it is bigger than this, log file is moved into history.<br>
-
-    --log-history=5<br>
-    Max number of 'old' logs to be kept.
-
-    --log-screenonly<br>
-    Only write to screen. This is by default disabled.
 */
+
+/**
+  @brief
+    The default logging file.
+*/
+#define XLOG_DEFAULT_FILE stderr
+
+/**
+  @brief
+    Disable logging. No logs will be shown.
+*/
+#define XLOG_DISABLED     -1
 
 /**
   @brief
     Logging level of fatal errors. The system will typically halt on fatal errors.
 */
-#define XLOG_FATAL    0
+#define XLOG_FATAL        0
 
 /**
   @brief
     Logging level of errors. The system will run incorrectly on these errors.
 */
-#define XLOG_ERROR    1
+#define XLOG_ERROR        1
 
 /**
   @brief
     Logging level of warnings.
 */
-#define XLOG_WARNING  2
+#define XLOG_WARNING      2
 
 /**
   @brief
     Logging level of informations.
 */
-#define XLOG_INFO     3
+#define XLOG_INFO         3
 
 /**
   @brief
     Logging level of debug messages.
 */
-#define XLOG_DEBUG    4
+#define XLOG_DEBUG        4
 
 /**
   @brief
     Logging level 5. User defined.
 */
-#define XLOG_LEVEL5   5
+#define XLOG_LEVEL5       5
 
 /**
   @brief
     Logging level 6. User defined.
 */
-#define XLOG_LEVEL6   6
+#define XLOG_LEVEL6       6
 
 /**
   @brief
     Logging level 7. User defined.
 */
-#define XLOG_LEVEL7   7
+#define XLOG_LEVEL7       7
+
+/**
+  @brief
+    The actions to control the logging system.
+*/
+typedef enum {
+  XLOG_CTL_ADD,           ///< @brief Create a new logger.
+  XLOG_CTL_REMOVE,        ///< @brief Remove a logger.
+  XLOG_CTL_ENABLE,        ///< @brief Enable a logger.
+  XLOG_CTL_DISABLE,       ///< @brief Disable a logger.
+  XLOG_CTL_SET_FILE,      ///< @brief Set the logger's file pointer.
+  XLOG_CTL_SET_LEVELS,    ///< @brief Set the logger's log levels.
+  XLOG_CTL_SET_SIZE,      ///< @brief Set the logger's max log size.
+  XLOG_CTL_SET_HISTORY    ///< @brief Set the logger's history count.
+} xlog_ctl_action;
+
 
 /**
   @brief
     Initialize logging system, parse command line args concerning the log system.
+
+  The logging system uses the following parameters:<br>
+  --log-level=2<br>
+  Loging levels are: 0 (fatal), 1 (error), 2 (warning), 3 (info), 4 (debug), 5~7(user defined).<br>
+
+  --log-dir=.<br>
+  Where all the logs and log history will be saved.<br>
+
+  --log-maxsize=10M<br>
+  Max file size of the log, if it is bigger than this, log file is moved into history.<br>
+
+  --log-history=5<br>
+  Max number of 'old' logs to be kept.
+
+  --log-screenonly<br>
+  Only write to screen. This is by default disabled.
 
   @param argc
     The number of command line args. This is provided to main().
   @param argv
     The array of command line args. This is provided to main().
 
+  @deprecated
+    This function is deprecated, use xlog_add() instead.
+
   @return
     Whether the logging system was correctly started.
+    
 */
 xsuccess xlog_init(int argc, char* argv[]);
+
+/**
+  @brief
+    General interface to control the logging system.
+  
+  @param logger_name
+    The logger that we are dealing with.
+  @param action
+    The control action we gonna take.
+  
+  @return
+    If the control action is successful.
+*/
+xsuccess xlog_ctl(const char* logger_name, xlog_ctl_action action, ...);
+
+/**
+  @brief
+    Create a new logger.
+  
+  This is actually a macro around xlog_ctl(), using the @c XLOG_CTL_ADD action.
+  
+  @param logger_name
+    Name of the new logger.
+
+  @return
+    Whether the logger is successfully created.
+*/
+#define xlog_add(logger_name) xlog_ctl(logger_name, XLOG_CTL_ADD)
+
+
+/**
+  @brief
+    Remove an existing logger.
+  
+  This is actually a macro around xlog_ctl(), using the @c XLOG_CTL_REMOVE action.
+  
+  @param logger_name
+    Name of the new logger.
+
+  @return
+    Whether the logger is successfully removed.
+*/
+#define xlog_remove(logger_name) xlog_ctl(logger_name, XLOG_CTL_REMOVE)
 
 
 /**
@@ -104,9 +179,38 @@ xsuccess xlog_init(int argc, char* argv[]);
     The logging level, could be 0 (fatal), 1 (error), 2 (warning), 3 (info), 4 (debug), 5~7 (user defined).
   @param fmt
     The formatting string.
+  @param code_fn
+    The code file name containing logging code.
+  @param code_ln
+    The line number of logging code.
+  
+  @warning
+    This function is not intended to be used directly. Use xlog() instead.
+  
+  @return
+    Whether the log was written successfully.
 */
+xsuccess xlog_real(int level, const char* code_fn, int code_ln, const char* fmt, ...);
+
+/**
+  @brief
+    Write logs of a certain logging level.
+  
+  @param level
+    The logging level, could be 0 (fatal), 1 (error), 2 (warning), 3 (info), 4 (debug), 5~7 (user defined).
+  @param fmt
+    The formatting string.
+    
+  @return
+    Whether the log was written successfully.
+*/
+#define xlog(level, fmt, ...) xlog_real(level, __FILE__, __LINE__, fmt, ## __VA_ARGS__)
+
+/*
 #define xlog(level, fmt, ...) { \
-  if (level <= XLOG_LEVEL7) { \
+  if (level == XLOG_DISABLED) { \
+     \
+  } else if (level <= XLOG_LEVEL7) { \
     xlog_start_record(level); \
     printf(fmt, ## __VA_ARGS__); \
     if (xlog_is_screen_only() == XFALSE && xlog_get_log_fp() != NULL) {  \
@@ -117,6 +221,7 @@ xsuccess xlog_init(int argc, char* argv[]);
     fprintf(stderr, "[error] no such log level: %d\n", level);  \
   } \
 };
+*/
 
 /**
   @brief
@@ -177,7 +282,7 @@ xsuccess xlog_init(int argc, char* argv[]);
     Whether the log was correctly written.
 
   @warning
-    Do not call this function directly. Use xlog() instead!
+    This function is not intended to be used by users!
 */
 void xlog_start_record(int level);
 
@@ -192,7 +297,7 @@ void xlog_start_record(int level);
     Whether the log was correctly written.
 
   @warning
-    Do not call this function directly. Use xlog() instead!
+    This function is not intended to be used by users!
 */
 void xlog_end_record(const char* fmt);
 
@@ -204,7 +309,7 @@ void xlog_end_record(const char* fmt);
     The file pointer.
 
   @warning
-    Do not call this function directly. Use xlog() instead!
+    This function is not intended to be used by users!
 */
 FILE* xlog_get_log_fp();
 
@@ -216,7 +321,7 @@ FILE* xlog_get_log_fp();
     XTRUE if logs are written to stdout only.
 
   @warning
-    Do not call this function directly. Use xlog() instead!
+    This function is not intended to be used by users!
 */
 xbool xlog_is_screen_only();
 
