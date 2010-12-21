d9d3b9db54980050aefee67e7e8e9999917798b8
Santa Zhang <santa1987@gmail.com>
2010-12-21 22:23:14 +0800

    [auto] update nova/tools/server_side/xdk/xlog.c to newest version

--- tools/server_side/xdk/xlog.c
+++ tools/server_side/xdk/xlog.c
@@ -18,6 +18,8 @@
 
 #define DEFAULT_XLOGGER_PATTERN "[%Y.%m.%d %H:%M:%S] [%e] %l"
 
+static const char* log_level_names[] = {"fatal", "error", "warning", "info", "debug", "level5", "level6", "level7"};
+
 // this is the mutex which protects the logging system
 static pthread_mutex_t logging_lock = PTHREAD_MUTEX_INITIALIZER;
 
@@ -44,7 +46,7 @@ typedef struct {
   char* fn; // file name for the logger file
   // a series of files fn.1, fn.2, .. etc will be created, when the base log file is too big
 
-  FILE* fp; // pointer to the log file pointer, by default, XLOG_DEFAULT_FILE
+  FILE* fp; // pointer to the log file pointer
 
   xbool enabled;  // whether the logger is enabled
   int levels; // a flag indicating which levels should the logger run, by default, fatal~info
@@ -76,28 +78,49 @@ static char* get_log_basename(char* argv0) {
   return log_basename;
 }
 
+
+static void destroy_xlogger(xlogger_ty* lg) {
+  if (lg->special == XFALSE) {
+    fclose(lg->fp);
+    lg->fp = NULL;
+  }
+  if (lg->fn != NULL) {
+    free(lg->fn);
+  }
+  free(lg->name);  // also used as key, created by strdup, so we use free instead of xfree here
+  free(lg->pattern);
+  xfree(lg);  // xfree the logger, because it is initialized using xmalloc()
+}
+
+static void xloggers_free(void* key, void* value) {
+  // the key is also lg->name, so we do nothing on key
+  destroy_xlogger((xlogger_ty *) value);
+}
+
+// make sure the logging system is initialized, ready to use
+static inline xsuccess xlog_ensure_initialized() {
+  if (g_xloggers == NULL) {
+    g_xloggers = xhash_new(xhash_hash_cstr, xhash_eql_cstr, xloggers_free);
+  }
+  return XSUCCESS;
+}
+
+
+void xlog_finalize() {
+  xlog_ensure_initialized();
+  pthread_mutex_lock(&logging_lock);
+  xhash_delete(g_xloggers);
+  g_xloggers = NULL;
+  pthread_mutex_unlock(&logging_lock);
+}
+
 xsuccess xlog_init(int argc, char* argv[]) {
   xsuccess ret = XSUCCESS;
   xoption xopt = xoption_new();
   xstr log_fn = xstr_new();
 
   xoption_parse(xopt, argc, argv);
-
-  // by default, create a console logging, and a file logging
-  xlog_add("default-console");
-  xlog_add("default-file");
-
-  if (xoption_has(xopt, "log-level")) {
-    int i;
-    int log_level_flag = 0;
-    int log_level = atoi(xoption_get(xopt, "log-level"));
-    for (i = 0; i <= log_level; i++) {
-      log_level_flag |= (1 << i);
-    }
-    xlog_ctl("default-console", XLOG_CTL_SET_LEVELS, log_level_flag);
-    xlog_ctl("default-file", XLOG_CTL_SET_LEVELS, log_level_flag);
-  }
-
+  xlog_add("stderr", NULL);
   if (xoption_has(xopt, "log-dir")) {
     xstr log_folder = xstr_new();
     xstr log_fn_abs = xstr_new();
@@ -118,20 +141,29 @@ xsuccess xlog_init(int argc, char* argv[]) {
     xstr_printf(log_fn, "%s.log", argv[0]);
   }
 
+  if (xoption_has(xopt, "log-screenonly") == XFALSE) {
+    xlog_add("default-log-file", xstr_get_cstr(log_fn));
+  }
+
+  if (xoption_has(xopt, "log-level")) {
+    int i;
+    int log_level_flag = 0;
+    int log_level = atoi(xoption_get(xopt, "log-level"));
+    for (i = 0; i <= log_level; i++) {
+      log_level_flag |= (1 << i);
+    }
+    xlog_ctl("stderr", XLOG_CTL_SET_LEVELS, log_level_flag);
+    xlog_ctl("default-log-file", XLOG_CTL_SET_LEVELS, log_level_flag);
+  }
+
   if (xoption_has(xopt, "log-maxsize")) {
     int log_max_size = xfilesystem_parse_filesize(xoption_get(xopt, "log-maxsize"));
-    xlog_ctl("default-file", XLOG_CTL_SET_SIZE, log_max_size);
+    xlog_ctl("default-log-file", XLOG_CTL_SET_SIZE, log_max_size);
   }
 
   if (xoption_has(xopt, "log-history")) {
     int log_history = atoi(xoption_get(xopt, "log-history"));
-    xlog_ctl("default-file", XLOG_CTL_SET_HISTORY, log_history);
-  }
-
-  if (xoption_has(xopt, "log-screenonly")) {
-    xlog_remove("default-file");
-  } else {
-    xlog_ctl("default-file", XLOG_CTL_SET_FILE, xstr_get_cstr(log_fn));
+    xlog_ctl("default-log-file", XLOG_CTL_SET_HISTORY, log_history);
   }
 
   xstr_delete(log_fn);
@@ -140,29 +172,7 @@ xsuccess xlog_init(int argc, char* argv[]) {
 }
 
 
-static void xloggers_free(void* key, void* value) {
-  // the key is also lg->name, so we do nothing on key
-  xlogger_ty* lg = (xlogger_ty *) value;
-  if (lg->special == XFALSE) {
-    fclose(lg->fp);
-  }
-  if (lg->fn != NULL) {
-    free(lg->fn);
-  }
-  free(lg->name);  // also used as key, created by strdup, so we use free instead of xfree here
-  free(lg->pattern);
-  xfree(lg);  // xfree the logger, because it is initialized using xmalloc()
-}
-
-// make sure the logging system is initialized, ready to use
-static inline xsuccess xlog_ensure_initialized() {
-  if (g_xloggers == NULL) {
-    g_xloggers = xhash_new(xhash_hash_cstr, xhash_eql_cstr, xloggers_free);
-  }
-  return XSUCCESS;
-}
-
-static xsuccess xlog_ctl_add(const char* logger_name) {
+static xsuccess xlog_ctl_add(const char* logger_name, const char* log_fn) {
   xsuccess ret = xlog_ensure_initialized();
   xlogger_ty* lg;
   if (ret == XSUCCESS) {
@@ -174,18 +184,35 @@ static xsuccess xlog_ctl_add(const char* logger_name) {
     }
   }
   if (ret == XSUCCESS) {
-    // by default, the logger file is XLOG_DEFAULT_FILE
     lg = xmalloc_ty(1, xlogger_ty);
     lg->name = strdup(logger_name);
     lg->pattern = strdup(DEFAULT_XLOGGER_PATTERN);
     lg->fn = NULL;  // no file
-    lg->fp = XLOG_DEFAULT_FILE;
+    lg->fp = NULL;
+    if (strcmp(logger_name, "stderr") == 0) {
+      lg->fp = stderr;
+      lg->special = XTRUE;
+    } else if (strcmp(logger_name, "stdout") == 0) {
+      lg->fp = stdout;
+      lg->special = XTRUE;
+    } else {
+      lg->special = XFALSE;
+      lg->fn = strdup(log_fn);
+      lg->fp = fopen(lg->fn, "a");
+      if (lg->fp == NULL) {
+        // failed to open file
+        ret = XFAILURE;
+      }
+    }
     lg->enabled = XTRUE;
-    lg->levels = 0x7; // fatal,debug,warning
-    lg->special = XTRUE;
+    lg->levels = 0xF; // fatal,error,warning,info
     lg->max_size = 10 * 1024 * 1024;  // default value
     lg->history = 5;  // default value
-    xhash_put(g_xloggers, lg->name, lg);
+    if (ret == XSUCCESS) {
+      xhash_put(g_xloggers, lg->name, lg);
+    } else {
+      destroy_xlogger(lg);
+    }
   }
   return ret;
 }
@@ -240,6 +267,9 @@ static xsuccess xlog_ctl_set_file(const char* logger_name, const char* fn) {
     // check if name already used
     lg = xhash_get(g_xloggers, logger_name);
     if (lg != NULL) {
+      if (lg->fn != NULL) {
+        free(lg->fn);
+      }
       lg->fn = strdup(fn);
       lg->fp = fp;
       lg->special = XFALSE;
@@ -306,7 +336,8 @@ xsuccess xlog_ctl(const char* logger_name, xlog_ctl_action action, ...) {
 
   switch(action) {
   case XLOG_CTL_ADD:
-    ret = xlog_ctl_add(logger_name);
+    sval = va_arg(argp, char *);
+    ret = xlog_ctl_add(logger_name, sval);
     break;
   case XLOG_CTL_REMOVE:
     ret = xlog_ctl_remove(logger_name);
@@ -335,7 +366,7 @@ xsuccess xlog_ctl(const char* logger_name, xlog_ctl_action action, ...) {
     break;
   default:
     // action not understood
-    fprintf(XLOG_DEFAULT_FILE, "xlog_clt action not understood! action id = %d\n", action);
+    fprintf(stderr, "xlog_clt action not understood! action id = %d\n", action);
     ret = XFAILURE;
     break;
   }
@@ -362,8 +393,6 @@ static xsuccess rotate_log(xlogger_ty* lg) {
   fclose(lg->fp);
   lg->fp = NULL;
 
-  //xstr_append_cstr(current_log_fn, ".log");
-
   for (i = 1; i <= lg->history; i++) {
     xstr detect_log_fn = xstr_new();
     xstr_printf(detect_log_fn, "%s.%d", xstr_get_cstr(current_log_fn), i);
@@ -375,7 +404,6 @@ static xsuccess rotate_log(xlogger_ty* lg) {
 
   if (i <= lg->history) {
     xstr history_log_fn = xstr_new();
-      printf("HERE %s %s\n", lg->fn, xstr_get_cstr(current_log_fn));
     xstr_printf(history_log_fn, "%s.%d", xstr_get_cstr(current_log_fn), i);
     rename(xstr_get_cstr(current_log_fn), xstr_get_cstr(history_log_fn));
     xstr_delete(history_log_fn);
@@ -406,9 +434,10 @@ static xbool do_log_visitor(const void* key, void* value, void* args) {
   do_log_visitor_param* p_param = (do_log_visitor_param *) args;
   int level = p_param->level;
   xlogger_ty* lg = (xlogger_ty *) value;
-
+  if (lg->special == XFALSE) {
+    assert(lg->fp != NULL);
+  }
   if (lg->enabled == XTRUE && ((lg->levels >> level) & 0x1) == 1) {
-    char* log_level_names[] = {"fatal", "error", "warning", "info", "debug", "level5", "level6", "level7"};
     char* p = lg->pattern;
     char timestr_buf[32];
     struct tm* tm_struct;
@@ -484,27 +513,30 @@ static xbool do_log_visitor(const void* key, void* value, void* args) {
 }
 
 xsuccess xlog_real(int level, const char* code_fn, int code_ln, const char* fmt, ...) {
-  xsuccess ret = XSUCCESS;
-  va_list argp;
-  do_log_visitor_param param;
-  xstr all_logs = xstr_new();
-  xvec lines;
-  int i;
-  va_start(argp, fmt);
-  pthread_mutex_lock(&logging_lock);
-  xstr_vprintf(all_logs, fmt, argp);
-  lines = xstr_split_xvec(all_logs, "\n");
-  param.level = level;
-  param.code_fn = code_fn;
-  param.code_ln = code_ln;
-  for (i = 0; i < xvec_size(lines); i++) {
-    // call the function for each line
-    param.log_line = xvec_get(lines, i);
-    xhash_visit(g_xloggers, do_log_visitor, &param);
+  xsuccess ret = xlog_ensure_initialized();
+  if (ret == XSUCCESS) {
+    va_list argp;
+    do_log_visitor_param param;
+    xstr all_logs = xstr_new();
+    xvec lines;
+    int i;
+    va_start(argp, fmt);
+    pthread_mutex_lock(&logging_lock);
+    xstr_vprintf(all_logs, fmt, argp);
+    lines = xstr_split_xvec(all_logs, "\n");
+    param.level = level;
+    param.code_fn = code_fn;
+    param.code_ln = code_ln;
+    assert (xhash_size(g_xloggers) > 0);
+    for (i = 0; i < xvec_size(lines); i++) {
+      // call the function for each line
+      param.log_line = xvec_get(lines, i);
+      xhash_visit(g_xloggers, do_log_visitor, &param);
+    }
+    xvec_delete(lines);
+    pthread_mutex_unlock(&logging_lock);
+    va_end(argp);
+    xstr_delete(all_logs);
   }
-  xvec_delete(lines);
-  pthread_mutex_unlock(&logging_lock);
-  va_end(argp);
-  xstr_delete(all_logs);
   return ret;
 }
