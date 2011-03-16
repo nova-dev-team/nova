d98e4f6909dfa6d6a4ad8731124a676d35b44fe6
Santa Zhang <santa1987@gmail.com>
2011-03-16 20:56:34 +0800

    added agent & vncproxy src folder

---
+++ project/src/agent/nova/agent/NovaAgent.java
@@ -0,0 +1,9 @@
+package nova.agent;
+
+public class NovaAgent {
+
+	public static void main(String[] args) {
+		System.out.println("This shall be done!");
+	}
+
+}
---
+++ project/src/vncproxy/nova/vncproxy/NovaVNCProxy.java
@@ -0,0 +1,9 @@
+package nova.vncproxy;
+
+public class NovaVNCProxy {
+	
+	public static void main(String[] args) {
+		System.out.println("This shall be done!");
+	}
+
+}
