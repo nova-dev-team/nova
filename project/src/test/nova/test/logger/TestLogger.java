5357f3f82ba484d38cca562272e7b45d881fecfe
Santa Zhang <santa1987@gmail.com>
2011-03-24 22:07:28 +0800

    added junit & log4j

---
+++ project/src/test/nova/test/logger/TestLogger.java
@@ -0,0 +1,19 @@
+package nova.test.logger;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class TestLogger {
+
+	static Logger logger = Logger.getLogger(TestLogger.class.getName());
+
+	@Test
+	public void test() {
+		logger.debug("This is a debug message");
+		logger.error("This is an error message");
+		logger.fatal("This is a fatal message");
+		logger.info("This is an info");
+		logger.trace("This is a trace");
+	}
+
+}
