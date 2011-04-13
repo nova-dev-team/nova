04c13c0291139ffaded99ed72917dd617f7b59a3
earlycicada <gaotao1987@gmail.com>
2011-04-13 18:16:27 +0800

    Implementation of InstallProgressDaemon and test case

---
+++ project/src/agent/nova/agent/daemons/InstallProgressDaemon.java
@@ -0,0 +1,27 @@
+package nova.agent.daemons;
+
+import nova.agent.common.util.GlobalPara;
+import nova.agent.core.InstallProgress;
+import nova.common.util.SimpleDaemon;
+
+/**
+ * Daemon deal with installing softwares
+ * 
+ * @author gaotao1987@gmail.com
+ * 
+ */
+public class InstallProgressDaemon extends SimpleDaemon {
+
+	public InstallProgressDaemon() {
+		super(100);
+	}
+
+	@Override
+	protected void workOneRound() {
+
+		String downloadedSoftware = GlobalPara.downloadedBuffer.read();
+		InstallProgress insP = new InstallProgress(downloadedSoftware,
+				GlobalPara.myPath);
+		insP.run();
+	}
+}
