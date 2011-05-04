df2841bd6e3c67db6b6d8d5bed796032c553ab4d
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-05-04 16:53:08 +0800

    added worker generate vnodes status message class

--- project/src/worker/nova/worker/daemons/ReportVnodeStatusDeamon.java
+++ project/src/worker/nova/worker/daemons/ReportVnodeStatusDeamon.java
@@ -1,6 +1,14 @@
 package nova.worker.daemons;
 
+import java.util.ArrayList;
+
+import nova.common.service.SimpleAddress;
 import nova.common.util.SimpleDaemon;
+import nova.master.api.MasterProxy;
+import nova.master.models.Vnode;
+import nova.worker.NovaWorker;
+
+import org.apache.log4j.Logger;
 
 /**
  * Daemon thread that reports all current vnodes status to master node.
@@ -10,10 +18,24 @@ import nova.common.util.SimpleDaemon;
  */
 public class ReportVnodeStatusDeamon extends SimpleDaemon {
 
+	/**
+	 * Log4j logger.
+	 */
+	Logger logger = Logger.getLogger(ReportVnodeStatusDeamon.class);
+
+	SimpleAddress vAddr;
+	ArrayList<Vnode.Status> vnodesStatus;
+
 	@Override
 	protected void workOneRound() {
 
 		// TODO @shayf report actual vnodes status to master
+		if (this.isStopping() == false) {
+			MasterProxy master = NovaWorker.getInstance().getMaster();
+			if (master != null) {
+				master.sendVnodeStatus(vAddr, vnodesStatus);
+			}
+		}
 	}
 
 }
