2233347da4f9325be6a55dab9981aa2c66bede19
Santa Zhang <santa1987@gmail.com>
2011-05-05 23:17:14 +0800

    finished cleanup agent

--- project/src/agent/nova/agent/daemons/helper/ApplianceFetcher.java
+++ project/src/agent/nova/agent/daemons/helper/ApplianceFetcher.java
@@ -1,5 +1,9 @@
 package nova.agent.daemons.helper;
 
+import java.io.IOException;
+
 public abstract class ApplianceFetcher {
+
+	public abstract void fetch(String appName) throws IOException;
 
 }
--- project/src/agent/nova/agent/daemons/helper/FtpApplianceFetcher.java
+++ project/src/agent/nova/agent/daemons/helper/FtpApplianceFetcher.java
@@ -1,5 +1,7 @@
 package nova.agent.daemons.helper;
 
+import java.io.IOException;
+
 import nova.common.util.Conf;
 
 public class FtpApplianceFetcher extends ApplianceFetcher {
@@ -7,6 +9,12 @@ public class FtpApplianceFetcher extends ApplianceFetcher {
 	Conf conf;
 
 	public FtpApplianceFetcher(Conf conf) {
+
+	}
+
+	@Override
+	public void fetch(String appName) throws IOException {
+		// TODO @santa Auto-generated method stub
 
 	}
 
