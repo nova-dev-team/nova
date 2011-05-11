9a2a5cb38ed6a4de9be4bbab040887dfafe6c0a5
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-05-11 14:06:56 +0800

    modify VdiskPoolDaemon

--- project/src/worker/nova/worker/daemons/VdiskPoolDaemon.java
+++ project/src/worker/nova/worker/daemons/VdiskPoolDaemon.java
@@ -18,12 +18,72 @@ import org.apache.log4j.Logger;
  */
 public class VdiskPoolDaemon extends SimpleDaemon {
 
+	public VdiskPoolDaemon() {
+		super();
+		VdiskFiles = new VdiskFile[POOL_SIZE];
+		for (int i = 0; i < POOL_SIZE; i++) {
+			VdiskFiles[i] = new VdiskFile();
+			// System.out.println(VdiskFiles[i].getStat().toString());
+			VdiskFiles[i].setStatValue(i + 1);
+			System.out.println(VdiskFiles[i].getStat().toString());
+		}
+	}
+
+	public VdiskPoolDaemon(long sleepMilli) {
+		super(sleepMilli);
+		VdiskFiles = new VdiskFile[POOL_SIZE];
+		for (int i = 0; i < POOL_SIZE; i++) {
+			VdiskFiles[i].setStatValue(i + 1);
+		}
+	}
+
 	/**
 	 * Log4j logger.
 	 */
 	Logger log = Logger.getLogger(VdiskPoolDaemon.class);
 
 	static int POOL_SIZE = 5;
+
+	private static class VdiskFile {
+		Status stat;
+
+		public Status getStat() {
+			return stat;
+		}
+
+		public void setStat(Status stat) {
+			this.stat = stat;
+		}
+
+		public void setStatValue(int i) {
+			File image = new File(Utils.pathJoin(Utils.NOVA_HOME, "run",
+					"vdiskpool", "linux.img.pool." + Integer.toString(i)));
+			if (!image.exists()) {
+				this.stat = Status.NOT_EXIST;
+			} else {
+				this.stat = Status.AVAILABLE;
+			}
+		}
+
+		enum Status {
+			/**
+			 * The vdiskfile is not existed.
+			 */
+			NOT_EXIST,
+
+			/**
+			 * The vdiskfile is locked for writing
+			 */
+			LOCKED,
+
+			/**
+			 * The vdiskfile is available
+			 */
+			AVAILABLE,
+		}
+	}
+
+	VdiskFile[] VdiskFiles;
 
 	public static int getPOOL_SIZE() {
 		return POOL_SIZE;
@@ -36,18 +96,23 @@ public class VdiskPoolDaemon extends SimpleDaemon {
 	@Override
 	protected void workOneRound() {
 		final String path = Utils.pathJoin(Utils.NOVA_HOME, "run", "vdiskpool");
-		for (int i = POOL_SIZE; i > 0; i--) {
-			File image = new File(Utils.pathJoin(path, "linux.img.pool."
-					+ Integer.toString(i)));
-			if (!image.exists()) {
+		for (int i = POOL_SIZE - 1; i >= 0; i--) {
+			if (VdiskFiles[i].getStat().equals(VdiskFile.Status.NOT_EXIST)) {
+				VdiskFiles[i].setStat(VdiskFile.Status.LOCKED);
 				try {
-					System.out.println("copying file"
-							+ Utils.pathJoin(path,
-									"linux.img.pool." + Integer.toString(i)));
+					System.out
+							.println("file"
+									+ Utils.pathJoin(path, "linux.img.pool."
+											+ Integer.toString(i + 1))
+									+ VdiskFile.Status.NOT_EXIST.toString());
+					System.out
+							.println("copying file"
+									+ Utils.pathJoin(path, "linux.img.pool."
+											+ Integer.toString(i + 1)));
 					String sourceUrl = Utils.pathJoin(Utils.NOVA_HOME, "run",
 							"linux.img");
 					String destUrl = Utils.pathJoin(Utils.pathJoin(path,
-							"linux.img.pool." + Integer.toString(i)));
+							"linux.img.pool." + Integer.toString(i + 1)));
 					File sourceFile = new File(sourceUrl);
 					if (sourceFile.isFile()) {
 						FileInputStream input = new FileInputStream(sourceFile);
@@ -64,6 +129,9 @@ public class VdiskPoolDaemon extends SimpleDaemon {
 				} catch (IOException e) {
 					log.error("copy image fail", e);
 				}
+				VdiskFiles[i].setStat(VdiskFile.Status.AVAILABLE);
+			} else if (VdiskFiles[i].getStat().equals(VdiskFile.Status.LOCKED)) {
+				// TODO @shayf test if copy fails
 			}
 		}
 	}
