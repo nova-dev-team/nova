cb9f4886999e0ba0f44cc0ce90bad8dfc116302f
Zhao Xun <zhaoxun87@gmail.com>
2011-05-12 16:45:15 +0800

    modify http

--- project/src/master/nova/master/handler/MasterHttpHandler.java
+++ project/src/master/nova/master/handler/MasterHttpHandler.java
@@ -1,11 +1,13 @@
 package nova.master.handler;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 
 import nova.common.service.SimpleAddress;
 import nova.common.service.SimpleHttpHandler;
 import nova.common.util.Utils;
-import nova.master.models.Pnode;
 
 import org.apache.log4j.Logger;
 import org.jboss.netty.channel.ChannelHandlerContext;
@@ -41,16 +43,42 @@ public class MasterHttpHandler extends SimpleHttpHandler {
 	@Override
 	public String renderResult(DefaultHttpRequest req) {
 		// TODO @zhaoxun render results
-		Pnode pnode = new Pnode();
-		pnode.setIp("0.0.0.0");
-
 		String fpath = Utils.pathJoin(Utils.NOVA_HOME, "www", "master",
 				"index.html");
+		// Pnode pnode = new Pnode();
+		// pnode.setIp("0.0.0.0");
+
+		URL url = null;
+		try {
+			url = new URL("http://10.0.1.240:3000/" + req.getUri());
+		} catch (MalformedURLException e) {
+			log.error(e);
+		}
+
+		String act = getAction(url.getFile());
+		String query = url.getQuery();
+		Map<String, String> queryMap = getQueryMap(query);
+
 		HashMap<String, Object> values = new HashMap<String, Object>();
-
 		values.put("content", "NOVA MASTER");
-		values.put("pnode_info", pnode);
-
+		// values.put("pnode_info", urilfpath);
 		return Utils.expandTemplateFile(fpath, values);
+
+	}
+
+	public static Map<String, String> getQueryMap(String query) {
+		String[] params = query.split("&");
+		Map<String, String> map = new HashMap<String, String>();
+		for (String param : params) {
+			String name = param.split("=")[0];
+			String value = param.split("=")[1];
+			map.put(name, value);
+		}
+		return map;
+	}
+
+	public static String getAction(String actFile) {
+		String[] act = actFile.split("\\?");
+		return act[0].split("/")[1];
 	}
 }
