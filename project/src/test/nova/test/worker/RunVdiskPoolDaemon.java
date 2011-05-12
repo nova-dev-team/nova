3e4c1596b9bde31893b8b9d4b7e86ab4e837fa36
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-05-12 18:04:12 +0800

    check revoke and del

--- project/src/test/nova/test/worker/RunVdiskPoolDaemon.java
+++ project/src/test/nova/test/worker/RunVdiskPoolDaemon.java
@@ -46,7 +46,7 @@ public class RunVdiskPoolDaemon {
 		// svh.handleMessage(msg, ctx2, e2, xreply2);
 
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(20000);
 		} catch (InterruptedException ex) {
 			ex.printStackTrace();
 		}
