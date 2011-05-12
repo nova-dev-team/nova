121e3009a59eaca8743b24cc120946499fbd0349
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-05-12 19:17:59 +0800

    revoke message and handler and test

---
+++ project/src/worker/nova/worker/api/messages/RevokeImageMessage.java
@@ -0,0 +1,25 @@
+package nova.worker.api.messages;
+
+/**
+ * message for revoke image files and clean up pool
+ * 
+ * @author shayf
+ * 
+ */
+public class RevokeImageMessage {
+	public RevokeImageMessage(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
