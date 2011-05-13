b05fa64519f69b6fa4d113ceb256ecdeb1bf7478
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-05-13 14:45:39 +0800

    worker trying to connnect ftp, fail in linux

---
+++ project/src/test/nova/test/worker/TestFetchFtp.java
@@ -0,0 +1,31 @@
+package nova.test.worker;
+
+import java.io.IOException;
+
+import nova.agent.NovaAgent;
+import nova.agent.appliance.Appliance;
+import nova.agent.appliance.FtpApplianceFetcher;
+import nova.storage.NovaStorage;
+
+import org.junit.Test;
+
+public class TestFetchFtp {
+	@Test
+	public void testFetchFtp() {
+
+		NovaStorage.getInstance().startFtpServer();
+
+		FtpApplianceFetcher fp = new FtpApplianceFetcher();
+		try {
+
+			Appliance app = new Appliance("picture");
+			NovaAgent.getInstance().getAppliances().put("picture", app);
+			// app.setStatus(Status.CANCELLED);
+			fp.fetch(app);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		NovaStorage.getInstance().shutdown();
+		fp.deleteDir("", "picture");
+	}
+}
