e1ca6449e19278d65c0e182c2923aef107d7c312
Zhao Xun <zhaoxun87@gmail.com>
2011-05-17 15:21:45 +0800

    add

--- project/src/master/nova/master/handler/CreateVnodeHandler.java
+++ project/src/master/nova/master/handler/CreateVnodeHandler.java
@@ -3,6 +3,9 @@ package nova.master.handler;
 import nova.common.service.SimpleAddress;
 import nova.common.service.SimpleHandler;
 import nova.master.api.messages.CreateVnodeMessage;
+import nova.master.models.Pnode;
+import nova.master.models.Vnode;
+import nova.worker.api.WorkerProxy;
 
 import org.apache.log4j.Logger;
 import org.jboss.netty.channel.ChannelHandlerContext;
@@ -19,7 +22,25 @@ public class CreateVnodeHandler implements SimpleHandler<CreateVnodeMessage> {
 	public void handleMessage(CreateVnodeMessage msg,
 			ChannelHandlerContext ctx, MessageEvent e, SimpleAddress xreply) {
 		// TODO Auto-generated method stub
+		SimpleAddress vAddr = new SimpleAddress("10.0.2.1", 4000);
+		int pid = 1;
+
+		Vnode vnode = new Vnode();
+		vnode.setAddr(vAddr);
+		vnode.setPmachineId(pid);
+		vnode.setName(msg.vmName);
+		vnode.setCpuCount(msg.cpuCount);
+		vnode.setMemorySize(msg.memorySize);
+		vnode.setSoftList(msg.applianceList);
+		vnode.setCdrom(msg.vmImage);
+		vnode.setStatus(Vnode.Status.PREPARING);
+		vnode.save();
+		log.info("Created new vnode: " + vnode.getIp());
+
+		Pnode pnode = Pnode.findById(pid);
+		WorkerProxy wp = new WorkerProxy(pnode.getAddr());
+		wp.sendStartVnode("kvm", vAddr, "true", String.valueOf(msg.memorySize),
+				String.valueOf(msg.cpuCount), msg.vmImage, "false");
 
 	}
-
 }
