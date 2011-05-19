4104728269a21d93c687e8d7cdb0e6a87f4ef9c6
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-05-19 11:45:06 +0800

    register handlers

--- project/src/worker/nova/worker/handler/WorkerQueryPnodeInfoMessageHandler.java
+++ project/src/worker/nova/worker/handler/WorkerQueryPnodeInfoMessageHandler.java
@@ -1,5 +1,20 @@
 package nova.worker.handler;
 
-public class WorkerQueryPnodeInfoMessageHandler {
+import nova.common.service.SimpleAddress;
+import nova.common.service.SimpleHandler;
+import nova.worker.api.messages.QueryPnodeInfoMessage;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.MessageEvent;
+
+public class WorkerQueryPnodeInfoMessageHandler implements
+		SimpleHandler<QueryPnodeInfoMessage> {
+
+	@Override
+	public void handleMessage(QueryPnodeInfoMessage msg,
+			ChannelHandlerContext ctx, MessageEvent e, SimpleAddress xreply) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
