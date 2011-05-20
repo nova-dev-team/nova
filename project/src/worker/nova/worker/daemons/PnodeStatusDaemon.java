89fd4db484f9a713a3784729bfbffde12c226906
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-05-20 14:16:22 +0800

    add PnodeStatusDaemon

---
+++ project/src/worker/nova/worker/daemons/PnodeStatusDaemon.java
@@ -0,0 +1,24 @@
+package nova.worker.daemons;
+
+import nova.common.util.SimpleDaemon;
+import nova.master.api.MasterProxy;
+import nova.master.models.Pnode;
+import nova.worker.NovaWorker;
+
+public class PnodeStatusDaemon extends SimpleDaemon {
+
+	public static final long PNODE_STATUS_INTERVAL = 2000;
+
+	public PnodeStatusDaemon() {
+		super(PNODE_STATUS_INTERVAL);
+	}
+
+	@Override
+	protected void workOneRound() {
+		MasterProxy master = NovaWorker.getInstance().getMaster();
+		// NovaWorker.getInstance().getAddr()
+		master.sendPnodeStatus(NovaWorker.getInstance().getAddr(),
+				Pnode.Status.RUNNING);
+	}
+
+}
