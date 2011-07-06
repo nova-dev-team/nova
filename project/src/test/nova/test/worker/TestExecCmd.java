c6687325bd4cabdaf94622bab6eb2405c60f08d7
Sha Yefeng <shayf06@mails.tsinghua.edu.cn>
2011-07-06 17:05:31 +0800

    create bridge while worker starts

--- project/src/test/nova/test/worker/TestExecCmd.java
+++ project/src/test/nova/test/worker/TestExecCmd.java
@@ -13,22 +13,39 @@ public class TestExecCmd {
 		Logger log = Logger.getLogger(TestExecCmd.class);
 
 		Process p;
-		String cmd = "ifconfig";
-		System.out.println(cmd);
+		String[] cmds = { "ifconfig", "ls" };
+		// String[] cmds = {
+		// "ifconfig br0 down",
+		// "brctl delbr br0",
+		// "brctl addbr br0",
+		// "brctl setbridgeprio br0 0",
+		// "brctl addif br0 eth0",
+		// "ifconfig eth0 0.0.0.0",
+		// "ifconfig br0 " + Conf.getString("worker.bind_host")
+		// + " netmask " + Conf.getString("worker.bind_host"),
+		// "brctl sethello br0 1", "brctl setmaxage br0 4",
+		// "brctl setfd br0 4", "ifconfig br0 up",
+		// "route add default gw " + Conf.getString("worker.gateway") };
+		for (String cmd : cmds) {
+			System.out.println(cmd);
+		}
+
 		try {
-			p = Runtime.getRuntime().exec(cmd);
-			StreamGobbler errorGobbler = new StreamGobbler(p.getErrorStream(),
-					"ERROR");
-			errorGobbler.start();
-			StreamGobbler outGobbler = new StreamGobbler(p.getInputStream(),
-					"STDOUT");
-			outGobbler.start();
-			try {
-				if (p.waitFor() != 0) {
-					log.error("pack iso returned abnormal value!");
+			for (String cmd : cmds) {
+				p = Runtime.getRuntime().exec(cmd);
+				StreamGobbler errorGobbler = new StreamGobbler(
+						p.getErrorStream(), "ERROR");
+				errorGobbler.start();
+				StreamGobbler outGobbler = new StreamGobbler(
+						p.getInputStream(), "STDOUT");
+				outGobbler.start();
+				try {
+					if (p.waitFor() != 0) {
+						log.error("pack iso returned abnormal value!");
+					}
+				} catch (InterruptedException e1) {
+					log.error("pack iso process terminated", e1);
 				}
-			} catch (InterruptedException e1) {
-				log.error("pack iso process terminated", e1);
 			}
 		} catch (IOException e1) {
 			log.error("exec mkisofs cmd error!", e1);
