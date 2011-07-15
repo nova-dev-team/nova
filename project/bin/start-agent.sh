893d96bb6e3d7af861490a07eebbdc7cfdaa9e60
Santa Zhang <santa1987@gmail.com>
2011-07-15 15:13:46 +0800

    use start.py to boot Nova components

--- project/bin/start-agent.sh
+++ project/bin/start-agent.sh
@@ -19,53 +19,5 @@ script=`basename "$this"`
 bin=`cd "$bin"; pwd`
 this="$bin/$script"
 
-export NOVA_HOME=`dirname "$this"`/..
-
-export NOVA_CONF_DIR="$NOVA_HOME/conf"
-export NOVA_DB_DIR="$NOVA_HOME/db"
-
-# some Java parameters
-if [ "$JAVA_HOME" != "" ]; then
-  #echo "run java in $JAVA_HOME"
-  export JAVA_HOME=$JAVA_HOME
-fi
-  
-if [ "$JAVA_HOME" = "" ]; then
-  echo "Error: JAVA_HOME is not set."
-  #exit 1
-fi
-
-# CLASSPATH initially contains $NOVA_CONF_DIR and $NOVA_DB_DIR
-export CLASSPATH="$NOVA_CONF_DIR:$NOVA_DB_DIR"
-export CLASSPATH=${CLASSPATH}:$JAVA_HOME/lib/tools.jar
-
-# so that filenames w/ spaces are handled correctly in loops below
-IFS=
-
-# for developers, add Nova classes to CLASSPATH
-if [ -d "$NOVA_HOME/bin/build" ]; then
-  export CLASSPATH=${CLASSPATH}:$NOVA_HOME/bin/build
-fi
-for f in $NOVA_HOME/bin/nova-*.jar; do
-  export CLASSPATH=${CLASSPATH}:$f;
-done
-
-# add libs to CLASSPATH
-for f in $NOVA_HOME/lib/*.jar; do
-  CLASSPATH=${CLASSPATH}:$f;
-done
-
-export NOVA_LOG_DIR="$HADOOP_HOME/logs"
-export NOVA_LOGFILE='worker.log'
-
-# restore ordinary behaviour
-unset IFS
-
-export NOVA_OPTS="$NOVA_OPTS -Dnova.log.dir=$NOVA_LOG_DIR"
-export NOVA_OPTS="$NOVA_OPTS -Dnova.log.file=$NOVA_LOGFILE"
-export NOVA_OPTS="$NOVA_OPTS -Dnova.root.logger=${NOVA_ROOT_LOGGER:-INFO,console}"
-
-cd $NOVA_HOME
-java -server $NOVA_OPTS -classpath "$CLASSPATH" nova.agent.NovaAgent "$@"
-cd - > /dev/null
+python $bin/start.py agent
 
--- project/bin/start.py
+++ project/bin/start.py
@@ -1,3 +1,84 @@
 #!/usr/bin/env python
 
-print "This shall be done!"
+# Script to start Nova components
+# Santa Zhang, 2011-07
+
+import os
+import sys
+
+def print_help():
+  print "Startup script for Nova"
+  print "Usage: start.py [master|worker|agent|storge|client]"
+
+if len(sys.argv) < 2 or "help" in sys.argv[1]:
+  print_help()
+  exit()
+if sys.argv[1] == "master":
+  nova_module = "nova.master.NovaMaster"
+elif sys.argv[1] == "worker":
+  nova_module = "nova.worker.NovaWorker"
+elif sys.argv[1] == "agent":
+  nova_module = "nova.agent.NovaAgent"
+elif sys.argv[1] == "storage":
+  nova_module = "nova.storage.NovaStorage"
+elif sys.argv[1] == "client":
+  nova_module = "org.apache.pivot.wtk.DesktopApplicationContext nova.ui.NovaUI"
+else:
+  print_help()
+  exit(1)
+
+# this script file's abs path
+my_abs_path = os.path.abspath(os.path.join(os.getcwd(), __file__))
+
+bin_dir = os.path.split(my_abs_path)[0]
+
+nova_home = os.path.split(bin_dir)[0]
+
+f = open(os.path.join(nova_home, "../VERSION"))
+nova_ver = f.read().strip()
+print "[INFO] Nova version: %s" % nova_ver
+f.close()
+
+if os.path.exists(os.path.join(bin_dir, "nova-%s.jar" % nova_ver)) == False:
+  print "[ERROR] '%s' not found under '%s'" % ("nova-%s.jar" % nova_ver, bin_dir)
+  exit(1)
+
+lib_dir = os.path.join(nova_home, "lib")
+conf_dir = os.path.join(nova_home, "conf")
+db_dir = os.path.join(nova_home, "db")
+
+java_home = os.getenv("JAVA_HOME")
+if java_home == None:
+  print "[WARN] JAVA_HOME not set!"
+  # TODO use jre/ if JAVA_HOME not set
+
+class_path = "conf:db"
+
+orig_class_path = os.getenv("CLASSPATH")
+if orig_class_path != None:
+  class_path += ":%s" % orig_class_path
+if java_home != None:
+  class_path += ":%s" % java_home
+
+for fn in os.listdir(lib_dir):
+  if fn.startswith("."):
+    continue
+  if fn.lower().endswith(".jar"):
+    class_path += ":lib/%s" % fn
+
+class_path += ":bin/nova-%s.jar" % nova_ver
+
+# create dirs if necessary
+for dir_name in ["log", "run", "fuck/up"]:
+  dir_path = os.path.join(nova_home, dir_name)
+  if os.path.exists(dir_path) == False:
+    os.makedirs(dir_path)
+
+os.system(
+"""
+cd %s
+java -server -classpath "%s" %s
+""" % (nova_home, class_path, nova_module)
+)
+
+
