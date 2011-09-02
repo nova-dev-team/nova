5eca86ab2630d985b20bbbbf0b398d13bf276370
Zhao Xun <zhaoxun87@gmail.com>
2011-09-02 13:19:09 +0800

    add new testMakeDir

--- project/src/worker/nova/worker/daemons/WorkerHeartbeatDaemon.java
+++ project/src/worker/nova/worker/daemons/WorkerHeartbeatDaemon.java
@@ -27,6 +27,8 @@ public class WorkerHeartbeatDaemon extends SimpleDaemon {
 			MasterProxy master = NovaWorker.getInstance().getMaster();
 			if (master != null) {
 				master.sendPnodeHeartbeat();
+				System.out
+						.println("**********************************************************");
 			}
 		}
 	}
