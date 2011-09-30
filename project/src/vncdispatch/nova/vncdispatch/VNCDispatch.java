5932de9b41c8abc6489ea3cf99f154131ad88021
Santa Zhang <santa1987@gmail.com>
2011-09-30 23:09:11 +0800

    remove vncdispatch, it is to be implemented as a separated project

--- project/src/vncdispatch/nova/vncdispatch/VNCDispatch.java
+++
@@ -1,9 +0,0 @@
-package nova.vncdispatch;
-
-public class VNCDispatch {
-
-	public static void main(String[] args) {
-		System.out.println("This shall be done!");
-	}
-
-}
--- project/src/vncdispatch/nova/vncdispatch/api/VNCDispatchProxy.java
+++
@@ -1,5 +0,0 @@
-package nova.vncdispatch.api;
-
-public class VNCDispatchProxy {
-
-}
