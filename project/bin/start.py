8960128c122fb44b08f52c68b7b17158528e6e0e
Zhao Xun <zhaoxun87@gmail.com>
2011-10-19 12:37:27 +0800

    maybe modify start.py & add vncDeamon

--- project/bin/start.py
+++ project/bin/start.py
@@ -6,42 +6,33 @@
 import os
 import sys
 
-
 def print_help():
-    print "Startup script for Nova"
-    print "Usage: start.py [master|worker|agent|storge|client]"
-
-
-def merge_path(*paths):
-    if os.name == "posix":
-        sep = ":"
-    else:
-        sep = ";"
-    return sep.join(paths)
-
+  print "Startup script for Nova"
+  print "Usage: start.py [master|worker|agent|storge|client]"
 
 if len(sys.argv) < 2 or "help" in sys.argv[1]:
-    print_help()
-    exit()
+  print_help()
+  exit()
 if sys.argv[1] == "master":
-    nova_module = "nova.master.NovaMaster"
+  nova_module = "nova.master.NovaMaster"
 elif sys.argv[1] == "worker":
-    nova_module = "nova.worker.NovaWorker"
+  nova_module = "nova.worker.NovaWorker"
 elif sys.argv[1] == "agent":
-    nova_module = "nova.agent.NovaAgent"
+  nova_module = "nova.agent.NovaAgent"
 elif sys.argv[1] == "storage":
-    nova_module = "nova.storage.NovaStorage"
+  nova_module = "nova.storage.NovaStorage"
 elif sys.argv[1] == "client":
-    nova_module = "org.apache.pivot.wtk.DesktopApplicationContext nova.ui.NovaUI"
+  nova_module = "org.apache.pivot.wtk.DesktopApplicationContext nova.ui.NovaUI"
 else:
-    print_help()
-    exit(1)
+  print_help()
+  exit(1)
 
 # this script file's abs path
-my_abs_path = os.path.abspath(__file__)
+my_abs_path = os.path.abspath(os.path.join(os.getcwd(), __file__))
 
-bin_dir = os.path.dirname(my_abs_path)
-nova_home = os.path.abspath(os.path.join(bin_dir, ".."))
+bin_dir = os.path.split(my_abs_path)[0]
+
+nova_home = os.path.split(bin_dir)[0]
 
 f = open(os.path.join(nova_home, "../VERSION"))
 nova_ver = f.read().strip()
@@ -49,43 +40,44 @@ print "[INFO] Nova version: %s" % nova_ver
 f.close()
 
 if os.path.exists(os.path.join(bin_dir, "nova-%s.jar" % nova_ver)) == False:
-    print "[ERROR] '%s' not found under '%s'" % ("nova-%s.jar" % nova_ver, bin_dir)
-    exit(1)
-
+  print "[ERROR] '%s' not found under '%s'" % ("nova-%s.jar" % nova_ver, bin_dir)
+  exit(1)
+fuyfuyfuyyu
 lib_dir = os.path.join(nova_home, "lib")
 conf_dir = os.path.join(nova_home, "conf")
 db_dir = os.path.join(nova_home, "db")
 
 java_home = os.getenv("JAVA_HOME")
 if java_home == None:
-    # just give a warning, don't quit yet
-    # Mac's JAVA_HOME variable cannot be found, but Java works correctly
-    print "[WARN] JAVA_HOME not set!"
+  print "[WARN] JAVA_HOME not set!"
+  # TODO use jre/ if JAVA_HOME not set
 
-# set proper CLASSPATH variable
-class_path = merge_path("conf", "db")
+class_path = "conf;db"
 
 orig_class_path = os.getenv("CLASSPATH")
 if orig_class_path != None:
-    class_path = merge_path(class_path, orig_class_path)
-
+  class_path += ";%s" % orig_class_path
 if java_home != None:
-    class_path = merge_path(class_path, java_home)
+  class_path += ";%s" % java_home
 
 for fn in os.listdir(lib_dir):
-    if fn.startswith("."):
-        continue
-    if fn.lower().endswith(".jar"):
-        class_path = merge_path(class_path, "lib/%s" % fn)
+  if fn.startswith("."):
+    continue
+  if fn.lower().endswith(".jar"):
+    class_path += ";lib/%s" % fn
 
-class_path = merge_path(class_path, "bin/nova-%s.jar" % nova_ver)
-
+class_path += ";bin/nova-%s.jar" % nova_ver
 
 # create dirs if necessary
 for dir_name in ["log", "run"]:
-    dir_path = os.path.join(nova_home, dir_name)
-    if os.path.exists(dir_path) == False:
-        os.makedirs(dir_path)
+  dir_path = os.path.join(nova_home, dir_name)
+  if os.path.exists(dir_path) == False:
+    os.makedirs(dir_path)
 
-os.chdir(nova_home)
-os.system('java -server -classpath "%s" %s' % (class_path, nova_module))
+os.system("cd %s" % (nova_home))
+os.system("""java -server -classpath "%s" %s""" % (class_path, nova_module))
+#print "nova_home:%s" % nova_home
+#print "class_path:%s" % class_path
+#print "nova_module:%s" % nova_module
+print "++++++++++++++++++++++++++++++++++++"
+
