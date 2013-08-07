ce2db31204e935739e5c8990474fdf0918cf337b
eaglewatcher <wbeaglewatcher@gmail.com>
2013-08-07 19:50:48 +0800

    fix bug when vm operated not by nova, status is not updated

--- src/worker/nova/worker/daemons/VnodeStatusDaemon.java
+++ src/worker/nova/worker/daemons/VnodeStatusDaemon.java
@@ -43,109 +43,93 @@ public class VnodeStatusDaemon extends SimpleDaemon {
         allStatus.remove(uu);
     }
 
+    private void getStatus(String hypervisor) throws LibvirtException {
+        String[] strs = NovaWorker.getInstance()
+                .getConn(hypervisor + ":///system", false).listDefinedDomains();
+        for (int i = 0; i < strs.length; i++) {
+            Domain dom = NovaWorker.getInstance()
+                    .getConn(hypervisor + ":///system", false)
+                    .domainLookupByName(strs[i]);
+            if (dom != null) {
+                UUID uu = UUID.fromString(dom.getUUIDString());
+                String info = dom.getInfo().state.toString();
+                Vnode.Status vs = null;
+
+                if (info.equalsIgnoreCase("VIR_DOMAIN_BLOCKED")) {
+                    // the domain is blocked on resource
+                    vs = Vnode.Status.PAUSED;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_CRASHED")) {
+                    // the domain is crashed
+                    vs = Vnode.Status.CONNECT_FAILURE;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_NOSTATE")) {
+                    // the domain has no state
+                    vs = Vnode.Status.UNKNOWN;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_PAUSED")) {
+                    // the domain is paused by user
+                    vs = Vnode.Status.PAUSED;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_RUNNING")) {
+                    // the domain is running
+                    vs = Vnode.Status.RUNNING;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_SHUTDOWN")) {
+                    // the domain is being shut down
+                    vs = Vnode.Status.SHUTTING_DOWN;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_SHUTOFF")) {
+                    // the domain is shut off
+                    vs = Vnode.Status.SHUT_OFF;
+                }
+                allStatus.put(uu, vs);
+            }
+        }
+
+        int[] ids = NovaWorker.getInstance()
+                .getConn(hypervisor + ":///system", false).listDomains();
+        for (int i = 0; i < ids.length; i++) {
+            Domain dom = NovaWorker.getInstance()
+                    .getConn(hypervisor + ":///system", false)
+                    .domainLookupByID(ids[i]);
+            if (dom != null) {
+                UUID uu = UUID.fromString(dom.getUUIDString());
+                String info = dom.getInfo().state.toString();
+                Vnode.Status vs = null;
+
+                if (info.equalsIgnoreCase("VIR_DOMAIN_BLOCKED")) {
+                    // the domain is blocked on resource
+                    vs = Vnode.Status.PAUSED;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_CRASHED")) {
+                    // the domain is crashed
+                    vs = Vnode.Status.CONNECT_FAILURE;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_NOSTATE")) {
+                    // the domain has no state
+                    vs = Vnode.Status.UNKNOWN;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_PAUSED")) {
+                    // the domain is paused by user
+                    vs = Vnode.Status.PAUSED;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_RUNNING")) {
+                    // the domain is running
+                    vs = Vnode.Status.RUNNING;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_SHUTDOWN")) {
+                    // the domain is being shut down
+                    vs = Vnode.Status.SHUTTING_DOWN;
+                } else if (info.equalsIgnoreCase("VIR_DOMAIN_SHUTOFF")) {
+                    // the domain is shut off
+                    vs = Vnode.Status.SHUT_OFF;
+                }
+                allStatus.put(uu, vs);
+            }
+        }
+    }
+
     @Override
     protected void workOneRound() {
-        // TODO @shayf report actual vnodes status to master
         synchronized (NovaWorker.getInstance().getConnLock()) {
             String hyper = Conf.getString("hypervisor.engine").trim();
             try {
                 if (hyper.indexOf("kvm") >= 0) {
-                    String[] strs = NovaWorker.getInstance()
-                            .getConn("qemu:///system", false)
-                            .listDefinedDomains();
-                    for (int i = 0; i < strs.length; i++) {
-                        Domain dom = NovaWorker.getInstance()
-                                .getConn("qemu:///system", false)
-                                .domainLookupByName(strs[i]);
-                        if (dom != null) {
-                            UUID uu = UUID.fromString(dom.getUUIDString());
-                            String info = dom.getInfo().state.toString();
-                            Vnode.Status vs = null;
-                            // TODO @shayf discuss status and finish status enum
-                            /*
-                             * BOOT_FAILURE?, CONNECT_FAILURE, PREPARING,
-                             * RUNNING, SCHEDULING, SHUT_OFF, SHUTTING_DOWN,
-                             * PAUSED
-                             */
-                            if (info.equalsIgnoreCase("VIR_DOMAIN_BLOCKED")) {
-                                // the domain is blocked on resource
-                                vs = Vnode.Status.PAUSED;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_CRASHED")) {
-                                // the domain is crashed
-                                vs = Vnode.Status.CONNECT_FAILURE;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_NOSTATE")) {
-                                // the domain has no state
-                                vs = Vnode.Status.UNKNOWN;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_PAUSED")) {
-                                // the domain is paused by user
-                                vs = Vnode.Status.PAUSED;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_RUNNING")) {
-                                // the domain is running
-                                vs = Vnode.Status.RUNNING;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_SHUTDOWN")) {
-                                // the domain is being shut down
-                                vs = Vnode.Status.SHUTTING_DOWN;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_SHUTOFF")) {
-                                // the domain is shut off
-                                vs = Vnode.Status.SHUT_OFF;
-                            }
-                            allStatus.put(uu, vs);
-                        }
-                        // NovaWorker.getInstance().closeConnectToKvm();
-                    }
+                    getStatus("qemu");
                 }
                 if (hyper.indexOf("vstaros") >= 0) {
-                    String[] strs = NovaWorker.getInstance()
-                            .getConn("vstaros:///system", false)
-                            .listDefinedDomains();
-                    for (int i = 0; i < strs.length; i++) {
-                        Domain dom = NovaWorker.getInstance()
-                                .getConn("vstaros:///system", false)
-                                .domainLookupByName(strs[i]);
-                        if (dom != null) {
-                            UUID uu = UUID.fromString(dom.getUUIDString());
-                            String info = dom.getInfo().state.toString();
-                            Vnode.Status vs = null;
-                            if (info.equalsIgnoreCase("VIR_DOMAIN_BLOCKED")) {
-                                // the domain is blocked on resource
-                                vs = Vnode.Status.PAUSED;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_CRASHED")) {
-                                // the domain is crashed
-                                vs = Vnode.Status.CONNECT_FAILURE;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_NOSTATE")) {
-                                // the domain has no state
-                                vs = Vnode.Status.UNKNOWN;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_PAUSED")) {
-                                // the domain is paused by user
-                                vs = Vnode.Status.PAUSED;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_RUNNING")) {
-                                // the domain is running
-                                vs = Vnode.Status.RUNNING;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_SHUTDOWN")) {
-                                // the domain is being shut down
-                                vs = Vnode.Status.SHUTTING_DOWN;
-                            } else if (info
-                                    .equalsIgnoreCase("VIR_DOMAIN_SHUTOFF")) {
-                                // the domain is shut off
-                                vs = Vnode.Status.SHUT_OFF;
-                            }
-                            allStatus.put(uu, vs);
-                        }
-                        // NovaWorker.getInstance().closeConnectToKvm();
-                    }
+                    getStatus("vstaros");
                 }
-                // NovaWorker.getInstance().closeConnectToKvm();
 
             } catch (LibvirtException e) {
                 logger.error("libvirt connection fail", e);
