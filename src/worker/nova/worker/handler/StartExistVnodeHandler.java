e5bd9e1b3821de8128fb4f70342e5286475bfb75
Tianyu Chen <chentianyu@outlook.com>
2016-04-09 20:43:19 +0800

    update lxc restart domain handler; not tested

--- src/worker/nova/worker/handler/StartExistVnodeHandler.java
+++ src/worker/nova/worker/handler/StartExistVnodeHandler.java
@@ -23,34 +23,51 @@ public class StartExistVnodeHandler
     @Override
     public void handleMessage(StartExistVnodeMessage msg,
             ChannelHandlerContext ctx, MessageEvent e, SimpleAddress xreply) {
-        // TODO Auto-generated method stub
+        // get master instance
         NovaWorker.masteraddr = xreply;
         if (NovaWorker.getInstance().getMaster() == null || NovaWorker
                 .getInstance().getMaster().isConnected() == false) {
             NovaWorker.getInstance().registerMaster(xreply);
         }
+
+        // get virtual driver URI
         final String virtService;
         if (msg.hyper.equalsIgnoreCase("kvm")) {
             virtService = "qemu:///system";
         } else if (msg.hyper.equalsIgnoreCase("vstaros")) {
             virtService = "vstaros:///system";
+        } else if (msg.hyper.equalsIgnoreCase("lxc")) {
+            virtService = "lxc:///";
         } else {
             virtService = "some xen";
         }
 
         try {
+            // for debug
+            log.info("trying to restart domain with uuid " + msg.uuid);
+            // connect to the domain with the uuid
             Domain dom = null;
             synchronized (NovaWorker.getInstance().getConnLock()) {
                 dom = NovaWorker.getInstance().getConn(virtService, false)
                         .domainLookupByUUIDString(msg.uuid);
-                // NovaWorker.getInstance().closeConnectToKvm();
             }
             if (dom == null) {
                 VnodeStatusDaemon.putStatus(UUID.fromString(msg.uuid),
                         Vnode.Status.CONNECT_FAILURE);
-                log.error("cannot connect and start domain " + msg.uuid);
+                log.error("failed to connect to domain " + msg.uuid);
                 return;
             }
+
+            // temporary hack for lxc container
+            // for debug
+            log.info("domain status" + dom.isActive());
+            if (msg.hyper.equalsIgnoreCase("lxc")) {
+                log.info("lxc starting...");
+                dom.create();
+                return;
+            }
+
+            // modify the domain xml and update the vnc port
             String xml = dom.getXMLDesc(0);
             dom.undefine();
             int vncport = Utils.getFreePort();
@@ -59,12 +76,13 @@ public class StartExistVnodeHandler
             String talxml = xml
                     .substring(xml.indexOf("type='vnc' port='") + 21);
             xml = prexml + vncport + talxml;
+            // redefine domain
             Domain testDomain = NovaWorker.getInstance()
                     .getConn(virtService, false).domainDefineXML(xml);
             testDomain.create();
             VnodeStatusDaemon.putStatus(UUID.fromString(msg.uuid),
                     Vnode.Status.PREPARING);
-
+            // remap vnc port
             Utils.WORKER_VNC_MAP.put(msg.uuid, String.valueOf(vncport));
             NovaWorker.getInstance().getMaster().sendPnodeCreateVnodeMessage(
                     NovaWorker.getInstance().getAddr().getIp(), msg.vnodeid,
